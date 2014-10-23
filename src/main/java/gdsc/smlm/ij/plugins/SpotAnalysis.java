f993ef316de64841c7939776d1909d8dd2576e38
Alex Herbert <a.herbert@sussex.ac.uk>
2014-10-23 12:56:41 +0100

    Organise imports

--- src/main/java/gdsc/smlm/ij/plugins/SpotAnalysis.java
+++ src/main/java/gdsc/smlm/ij/plugins/SpotAnalysis.java
@@ -77,7 +77,6 @@ import java.util.LinkedList;
 import java.util.List;
 import java.util.Scanner;
 import java.util.TreeSet;
-import java.util.concurrent.ExecutionException;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.Future;
