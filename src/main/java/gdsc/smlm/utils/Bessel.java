55b803bdcc312c7e3ab3ef23ab5f46ed73007c3d
Alex Herbert <a.herbert@sussex.ac.uk>
2014-11-14 16:41:48 +0000

    Added modified bessel I1 required for probability model of Gamma distribution convolved with a Gaussian.

--- src/main/java/gdsc/smlm/utils/Bessel.java
+++ src/main/java/gdsc/smlm/utils/Bessel.java
@@ -93,4 +93,36 @@ public class Bessel
 		double value1 = J1(x);
 		return 2.0 * value1 / x + value0;
 	}
+
+	/**
+	 * Compute the modified Bessel function of the first kind.
+	 * 
+	 * @param x
+	 * 
+	 * @return the modified Bessel function I1
+	 */
+	public static double I1(final double x)
+	{
+		double ax, ans, y;
+
+		if ((ax = Math.abs(x)) < 3.75)
+		{
+			y = x / 3.75;
+			y *= y;
+			ans = ax *
+					(0.5 + y *
+							(0.87890594 + y *
+									(0.51498869 + y *
+											(0.15084934 + y * (0.2658733e-1 + y * (0.301532e-2 + y * 0.32411e-3))))));
+		}
+		else
+		{
+			y = 3.75 / ax;
+			ans = 0.2282967e-1 + y * (-0.2895312e-1 + y * (0.1787654e-1 - y * 0.420059e-2));
+			ans = 0.39894228 + y *
+					(-0.3988024e-1 + y * (-0.362018e-2 + y * (0.163801e-2 + y * (-0.1031555e-1 + y * ans))));
+			ans *= (Math.exp(ax) / Math.sqrt(ax));
+		}
+		return x < 0.0 ? -ans : ans;
+	}
 }
