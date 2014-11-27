1d789445657a84c8c15f7584ab15588da5f07ed4
Alex Herbert <a.herbert@sussex.ac.uk>
2014-11-27 17:21:35 +0000

    Created a PoissonGaussianFunction that models the probability density of the convolution of a Poisson distribution with a Gaussian.

---
+++ src/main/java/gdsc/smlm/fitting/function/PoissonGaussianFunction.java
@@ -0,0 +1,258 @@
+package gdsc.smlm.fitting.function;
+
+import org.apache.commons.math3.util.FastMath;
+
+/*----------------------------------------------------------------------------- 
+ * GDSC SMLM Software
+ * 
+ * Copyright (C) 2014 Alex Herbert
+ * Genome Damage and Stability Centre
+ * University of Sussex, UK
+ * 
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or
+ * (at your option) any later version.
+ *---------------------------------------------------------------------------*/
+
+/**
+ * Implements the probability density function for a Poisson-Gaussian Mixture. The Gaussian is assumed to have mean of
+ * zero.
+ * <p>
+ * The implementation uses the saddle-point approximation described in Snyder, et al (1995) Compensation for readout
+ * noise in CCD images. J.Opt. Soc. Am. 12, 272-283.
+ */
+public class PoissonGaussianFunction
+{
+	private static final double EPSILON = 1e-6;
+	private static final double NORMALISATION = 1 / Math.sqrt(2 * Math.PI);
+	private static final double LOG_NORMALISATION = Math.log(NORMALISATION);
+
+	/**
+	 * Number of Picard iterations to use
+	 */
+	private static final int NUM_PICARD = 3;
+
+	private boolean usePicardApproximation = false;
+	private final double mu;
+	private final double sigmasquared;
+
+	/**
+	 * @param mu
+	 *            The mean of the Poisson distribution
+	 * @param sigmasquared
+	 *            The variance of the Gaussian distribution
+	 */
+	private PoissonGaussianFunction(final double mu, final double sigmasquared)
+	{
+		if (mu <= 0)
+			throw new IllegalArgumentException("Poisson mean must be strictly positive");
+		if (sigmasquared <= 0)
+			throw new IllegalArgumentException("Gaussian variance must be strictly positive");
+		this.mu = mu;
+		this.sigmasquared = sigmasquared;
+	}
+
+	/**
+	 * @param mu
+	 *            The mean of the Poisson distribution
+	 * @param s
+	 *            The standard deviation of the Gaussian distribution
+	 * @throws IllegalArgumentException
+	 *             if the mean or variance is zero or below
+	 */
+	public static PoissonGaussianFunction createWithStandardDeviation(final double mu, final double s)
+	{
+		return new PoissonGaussianFunction(mu, s * s);
+	}
+
+	/**
+	 * @param mu
+	 *            The mean of the Poisson distribution
+	 * @param var
+	 *            The variance of the Gaussian distribution
+	 * @throws IllegalArgumentException
+	 *             if the mean or variance is zero or below
+	 */
+	public static PoissonGaussianFunction createWithVariance(final double mu, final double var)
+	{
+		return new PoissonGaussianFunction(mu, var);
+	}
+
+	/**
+	 * Get the probability of observation x
+	 * 
+	 * @param x
+	 *            The observation value
+	 * @return The probability
+	 */
+	public double probability(final double x)
+	{
+		return probability(x, mu, sigmasquared, usePicardApproximation);
+	}
+
+	/**
+	 * Get the log(p) of observation x
+	 * 
+	 * @param x
+	 *            The observation value
+	 * @return The log of the probability
+	 */
+	public double logProbability(final double x)
+	{
+		return logProbability(x, mu, sigmasquared, usePicardApproximation);
+	}
+
+	/**
+	 * Get the probability of observation x
+	 * 
+	 * @param x
+	 *            The observation value
+	 * @param mu
+	 *            The mean of the Poisson distribution
+	 * @param sigmasquared
+	 *            The variance of the Gaussian distribution
+	 * @param usePicardApproximation
+	 *            Use the Picard approximation for the initial saddle point. The default is Pade.
+	 * @return The probability
+	 */
+	public static double probability(final double x, final double mu, final double sigmasquared,
+			final boolean usePicardApproximation)
+	{
+		double saddlepoint = (usePicardApproximation) ? picard(x, mu, sigmasquared) : pade(x, mu, sigmasquared);
+		saddlepoint = newton_iteration(x, mu, sigmasquared, saddlepoint);
+		final double l = sp_approx(x, mu, sigmasquared, saddlepoint);
+		return FastMath.exp(l) * NORMALISATION;
+	}
+
+	/**
+	 * Get the probability of observation x
+	 * 
+	 * @param x
+	 *            The observation value
+	 * @param mu
+	 *            The mean of the Poisson distribution
+	 * @param sigmasquared
+	 *            The variance of the Gaussian distribution
+	 * @param usePicardApproximation
+	 *            Use the Picard approximation for the initial saddle point. The default is Pade.
+	 * @return The probability
+	 */
+	public static double logProbability(final double x, final double mu, final double sigmasquared,
+			final boolean usePicardApproximation)
+	{
+		double saddlepoint = (usePicardApproximation) ? picard(x, mu, sigmasquared) : pade(x, mu, sigmasquared);
+		saddlepoint = newton_iteration(x, mu, sigmasquared, saddlepoint);
+		final double l = sp_approx(x, mu, sigmasquared, saddlepoint);
+		return l + LOG_NORMALISATION;
+	}
+
+	/**
+	 * Return the initial saddle point estimated by the Pade approximation
+	 * 
+	 * @param x
+	 * @param mu
+	 * @param sigmasquared
+	 * @return The saddle point
+	 */
+	private static double pade(final double x, final double mu, final double sigmasquared)
+	{
+		final double bterm = x - 2 * sigmasquared - mu;
+
+		// Original code
+		//return -Math.log(0.5 * (bterm + Math.sqrt(bterm * bterm + 4 * mu * (2 * sigmasquared + x))) / mu);
+
+		// Check for negative log
+		final double argument_to_log = 0.5 * (bterm + Math.sqrt(bterm * bterm + 4 * mu * (2 * sigmasquared + x))) / mu;
+		if (argument_to_log <= 0)
+			// Revert to Taylor approximation
+			return (mu - x) / (mu + sigmasquared);
+		return -Math.log(argument_to_log);
+	}
+
+	/**
+	 * Return the initial saddle point estimated by the Picard approximation
+	 * 
+	 * @param x
+	 * @param mu
+	 * @param sigmasquared
+	 * @return The saddle point
+	 */
+	private static double picard(final double x, final double mu, final double sigmasquared)
+	{
+		// Use Taylor approximation to obtain the starting point for Picard iteration
+		final double taylor = (mu - x) / (mu + sigmasquared);
+		double saddlepoint = taylor;
+		for (int i = 0; i < NUM_PICARD; i++)
+		{
+			final double argument_to_log = mu / (x + sigmasquared * saddlepoint);
+			if (argument_to_log <= 0)
+				// Break out of loop if argument to log goes negative
+				return taylor;
+			saddlepoint = Math.log(argument_to_log);
+		}
+		return saddlepoint;
+	}
+
+	/**
+	 * Returns the saddlepoint found by Newton iteration for a given x, mu, sigmasquared and an initial estimate of the
+	 * saddle point (found with either the Pade or Picard approach)
+	 * 
+	 * @param x
+	 * @param mu
+	 * @param sigmasquared
+	 * @param initial_saddlepoint
+	 * @return The saddle point
+	 */
+	private static double newton_iteration(final double x, final double mu, final double sigmasquared,
+			final double initial_saddlepoint)
+	{
+		double change;
+		double saddlepoint = initial_saddlepoint;
+		do
+		{
+			final double mu_exp_minus_s = mu * FastMath.exp(-saddlepoint);
+			change = (x + sigmasquared * saddlepoint - mu_exp_minus_s) / (sigmasquared + mu_exp_minus_s);
+			saddlepoint -= change;
+		} while (FastMath.abs(change) > EPSILON * Math.abs(saddlepoint));
+		return saddlepoint;
+	}
+
+	/**
+	 * Return the saddlepoint approximation to the log of p(x,mu,sigmasquared) given the saddle point found by the
+	 * Newton iteration. Remember the sqrt(2*PI) factor has been left out.
+	 * 
+	 * @param x
+	 * @param mu
+	 * @param sigmasquared
+	 * @param saddlepoint
+	 * @return The saddlepoint approximation
+	 */
+	private static double sp_approx(final double x, final double mu, final double sigmasquared, final double saddlepoint)
+	{
+		final double mu_exp_minus_s = mu * FastMath.exp(-saddlepoint);
+		final double phi2 = sigmasquared + mu_exp_minus_s;
+		return -mu + (saddlepoint * (x + 0.5 * sigmasquared * saddlepoint)) + mu_exp_minus_s - 0.5 * Math.log(phi2);
+	}
+
+	/**
+	 * Return if using the Picard approximation for the initial saddle point
+	 * 
+	 * @return True if using the Picard approximation
+	 */
+	public boolean isUsePicardApproximation()
+	{
+		return usePicardApproximation;
+	}
+
+	/**
+	 * Specify whether to use the Picard approximation for the initial saddle point. The alternative is Pade.
+	 * 
+	 * @param usePicardApproximation
+	 *            True to use the Picard approximation
+	 */
+	public void setUsePicardApproximation(boolean usePicardApproximation)
+	{
+		this.usePicardApproximation = usePicardApproximation;
+	}
+}
---
+++ src/test/java/gdsc/smlm/fitting/function/PoissonGaussianFunctionTest.java
@@ -0,0 +1,158 @@
+package gdsc.smlm.fitting.function;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class PoissonGaussianFunctionTest
+{
+	double[] photons = { 0.25, 0.5, 1, 2, 4, 10, 100, 1000 };
+	double[] noise = { 1, 2, 4, 8 };
+
+	@Test
+	public void cumulativeProbabilityIsOneWithPicard()
+	{
+		for (double p : photons)
+			for (double s : noise)
+				cumulativeProbabilityIsOne(p, s, true);
+	}
+
+	@Test
+	public void cumulativeProbabilityIsOneWithPade()
+	{
+		for (double p : photons)
+			for (double s : noise)
+				cumulativeProbabilityIsOne(p, s, true);
+	}
+
+	@Test
+	public void probabilityMatchesLogProbability()
+	{
+		for (double p : photons)
+			for (double s : noise)
+			{
+				probabilityMatchesLogProbability(p, s, true);
+				probabilityMatchesLogProbability(p, s, false);
+			}
+	}
+
+	@Test
+	public void padeIsFaster()
+	{
+		double[] noise2 = new double[noise.length];
+		for (int i = 0; i < noise.length; i++)
+			noise2[i] = noise[i] * noise[i];
+
+		int N = 100;
+		double[][] x = new double[photons.length][N];
+		for (int i = 0; i < photons.length; i++)
+		{
+			final double p = photons[i] * 2 / N;
+			for (int j = 0; j < N; j++)
+			{
+				x[i][j] = p * j;
+			}
+		}
+
+		long t1 = getTime(noise2, N, x, true);
+		long t2 = getTime(noise2, N, x, false);
+
+		System.out.printf("Picard %d : Pade %d (%fx)\n", t1, t2, t1/(double)t2);
+		Assert.assertTrue(String.format("Picard %d > Pade %d", t1, t2), t2 < t1);
+	}
+
+	private long getTime(double[] noise2, int N, double[][] x, final boolean usePicard)
+	{
+		// Warm up
+		for (double s2 : noise2)
+		{
+			for (int i = 0; i < photons.length; i++)
+			{
+				final double p = photons[i];
+				for (int j = 0; j < N; j++)
+				{
+					PoissonGaussianFunction.probability(x[i][j], p, s2, usePicard);
+				}
+			}
+		}
+
+		// Time
+		long t1 = System.nanoTime();
+		for (double s2 : noise2)
+		{
+			for (int i = 0; i < photons.length; i++)
+			{
+				final double p = photons[i];
+				for (int j = 0; j < N; j++)
+				{
+					PoissonGaussianFunction.probability(x[i][j], p, s2, usePicard);
+				}
+			}
+		}
+		t1 = System.nanoTime() - t1;
+		return t1;
+	}
+
+	private void cumulativeProbabilityIsOne(final double mu, final double s, final boolean usePicard)
+	{
+		PoissonGaussianFunction f = PoissonGaussianFunction.createWithStandardDeviation(mu, s);
+		f.setUsePicardApproximation(usePicard);
+		double p = 0;
+
+		// Evaluate an initial range. 
+		// Gaussian should have >99% within +/- s
+		// Poisson will have mean mu with a variance mu. 
+		// At large mu it is approximately normal so use 3 sqrt(mu) for the range added to the mean
+		int min = (int) -Math.ceil(3 * s);
+		int max = (int) Math.ceil(mu + 3 * Math.sqrt(mu));
+		for (int x = min; x <= max; x++)
+		{
+			final double pp = f.probability(x);
+			//System.out.printf("x=%d, p=%f\n", x, pp);
+			p += pp;
+		}
+		if (p > 1.01)
+			Assert.fail("P > 1: " + p);
+
+		// We have most of the probability density. 
+		// Now keep evaluating up and down until no difference
+		final double changeTolerance = 1e-6;
+		for (int x = min - 1;; x--)
+		{
+			final double pp = f.probability(x);
+			//System.out.printf("x=%d, p=%f\n", x, pp);
+			p += pp;
+			if (pp / p < changeTolerance)
+				break;
+		}
+		for (int x = max + 1;; x++)
+		{
+			final double pp = f.probability(x);
+			//System.out.printf("x=%d, p=%f\n", x, pp);
+			p += pp;
+			if (pp / p < changeTolerance)
+				break;
+		}
+		Assert.assertEquals(String.format("mu=%f, s=%f", mu, s), 1, p, 0.02);
+	}
+
+	private void probabilityMatchesLogProbability(final double mu, final double s, final boolean usePicard)
+	{
+		PoissonGaussianFunction f = PoissonGaussianFunction.createWithStandardDeviation(mu, s);
+		f.setUsePicardApproximation(usePicard);
+
+		// Evaluate an initial range. 
+		// Gaussian should have >99% within +/- s
+		// Poisson will have mean mu with a variance mu. 
+		// At large mu it is approximately normal so use 3 sqrt(mu) for the range added to the mean
+		int min = (int) -Math.ceil(3 * s);
+		int max = (int) Math.ceil(mu + 3 * Math.sqrt(mu));
+		for (int x = min; x <= max; x++)
+		{
+			final double p = f.probability(x);
+			if (p == 0)
+				continue;
+			final double logP = f.logProbability(x);
+			Assert.assertEquals(Math.log(p), logP, 1e-3 * Math.abs(logP));
+		}
+	}
+}
