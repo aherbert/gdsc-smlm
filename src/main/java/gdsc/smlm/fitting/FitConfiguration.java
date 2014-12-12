446eade6536f7d39ef26044b85679c1b1b61184e
Alex Herbert <a.herbert@sussex.ac.uk>
2014-12-12 15:55:07 +0000

    Changed relative threshold to a smaller value to allow Powell optimizer to converge when the background level is high.

--- src/main/java/gdsc/smlm/fitting/FitConfiguration.java
+++ src/main/java/gdsc/smlm/fitting/FitConfiguration.java
@@ -68,8 +68,8 @@ public class FitConfiguration implements Cloneable
 	private int maxFunctionEvaluations = 1000;
 	private SearchMethod searchMethod = SearchMethod.POWELL; // Best for noisy data since gradients are unstable
 	private boolean gradientLineMinimisation = false;
-	private double relativeThreshold = 1e-6;
-	private double absoluteThreshold = 1e-10;
+	private double relativeThreshold = 1e-9;
+	private double absoluteThreshold = 1e-16;
 
 	private StoppingCriteria stoppingCriteria = null;
 	private GaussianFunction gaussianFunction = null;
