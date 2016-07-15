38fa558d5fb27f337fb8cc88cc64b09071692173
Alex Herbert <a.herbert@sussex.ac.uk>
2016-07-15 06:22:02 +0100

    Moved LUT methods to gdsc.core

--- src/main/java/gdsc/smlm/ij/plugins/DrawClusters.java
+++ src/main/java/gdsc/smlm/ij/plugins/DrawClusters.java
@@ -34,6 +34,7 @@ import ij.plugin.PlugIn;
 import ij.process.ByteProcessor;
 import ij.process.FloatPolygon;
 import ij.process.LUT;
+import ij.process.LUTHelper;
 
 import java.awt.Color;
 import java.awt.Rectangle;
@@ -47,8 +48,6 @@ public class DrawClusters implements PlugIn
 {
 	private static final String TITLE = "Draw Clusters";
 	private static final String[] sorts = new String[] { "None", "ID", "Time", "Size", "Length", "MSD", "Mean/Frame" };
-	private static final String[] luts = new String[] { "Red-Hot", "Ice", "Rainbow", "Fire", "Red-Yellow", "Red",
-			"Green", "Blue", "Cyan", "Magenta", "Yellow" };
 
 	private static String inputOption = "";
 	private static String title = "";
@@ -247,7 +246,7 @@ public class DrawClusters implements PlugIn
 
 		// Draw the traces as ROIs on an overlay
 		Overlay o = new Overlay();
-		LUT lut = createLUT();
+		LUT lut = LUTHelper.createLUT(DrawClusters.lut);
 		double scale = 256.0 / count;
 		if (isUseStackPosition)
 		{
@@ -314,7 +313,7 @@ public class DrawClusters implements PlugIn
 		gd.addChoice("Sort", sorts, sorts[sort]);
 		gd.addCheckbox("Spline_fit (traces only)", splineFit);
 		gd.addCheckbox("Use_stack_position", useStackPosition);
-		gd.addChoice("LUT", luts, luts[lut]);
+		gd.addChoice("LUT", LUTHelper.luts, LUTHelper.luts[lut]);
 
 		gd.showDialog();
 
@@ -335,181 +334,7 @@ public class DrawClusters implements PlugIn
 
 		return true;
 	}
-
-	/**
-	 * Build a custom LUT that helps show the classes
-	 * 
-	 * @return
-	 */
-	private LUT createLUT()
-	{
-		byte[] reds = new byte[256];
-		byte[] greens = new byte[256];
-		byte[] blues = new byte[256];
-		int nColors;
-		switch (lut)
-		{
-			case 0: // red-hot
-			default:
-				nColors = setColours(reds, greens, blues, Color.red, Color.yellow, Color.WHITE);
-				break;
-			case 1:
-				nColors = ice(reds, greens, blues);
-				break;
-			case 2:
-				nColors = rainbow(reds, greens, blues);
-				break;
-			case 3:
-				nColors = fire(reds, greens, blues);
-				break;
-			case 4: // red-yellow
-				nColors = setColours(reds, greens, blues, Color.red, Color.yellow);
-				break;
-			case 5:
-				nColors = setColours(reds, greens, blues, Color.red);
-				break;
-			case 6:
-				nColors = setColours(reds, greens, blues, Color.green);
-				break;
-			case 7:
-				nColors = setColours(reds, greens, blues, Color.blue);
-				break;
-			case 8:
-				nColors = setColours(reds, greens, blues, Color.cyan);
-				break;
-			case 9:
-				nColors = setColours(reds, greens, blues, Color.magenta);
-				break;
-			case 10:
-				nColors = setColours(reds, greens, blues, Color.yellow);
-				break;
-		}
-		if (nColors < 256)
-			interpolate(reds, greens, blues, nColors);
-		return new LUT(reds, greens, blues);
-	}
-
-	private int rainbow(byte[] reds, byte[] greens, byte[] blues)
-	{
-		// Using HSV vary the Hue from 300 (magenta) to Red (0)
-		int n = 0;
-		for (int h = 300; h >= 0; h -= 2)
-		{
-			Color c = Color.getHSBColor(h / 360.0f, 1, 1);
-			reds[n] = (byte) c.getRed();
-			greens[n] = (byte) c.getGreen();
-			blues[n] = (byte) c.getBlue();
-			n++;
-		}
-		return n;
-	}
-
-	private int setColours(byte[] reds, byte[] greens, byte[] blues, Color... colours)
-	{
-		int n = 0;
-		if (colours.length == 1)
-		{
-			reds[n] = (byte) (colours[0].getRed() / 2);
-			greens[n] = (byte) (colours[0].getGreen() / 2);
-			blues[n] = (byte) (colours[0].getBlue() / 2);
-			n++;
-		}
-
-		for (Color colour : colours)
-		{
-			reds[n] = (byte) colour.getRed();
-			greens[n] = (byte) colour.getGreen();
-			blues[n] = (byte) colour.getBlue();
-			n++;
-		}
-		return n;
-	}
-
-	/**
-	 * Copied from ij.plugin.LutLoader
-	 * 
-	 * @param reds
-	 * @param greens
-	 * @param blues
-	 * @return
-	 */
-	private int ice(byte[] reds, byte[] greens, byte[] blues)
-	{
-		int[] r = { 0, 0, 0, 0, 0, 0, 19, 29, 50, 48, 79, 112, 134, 158, 186, 201, 217, 229, 242, 250, 250, 250, 250,
-				251, 250, 250, 250, 250, 251, 251, 243, 230 };
-		int[] g = { 156, 165, 176, 184, 190, 196, 193, 184, 171, 162, 146, 125, 107, 93, 81, 87, 92, 97, 95, 93, 93,
-				90, 85, 69, 64, 54, 47, 35, 19, 0, 4, 0 };
-		int[] b = { 140, 147, 158, 166, 170, 176, 209, 220, 234, 225, 236, 246, 250, 251, 250, 250, 245, 230, 230, 222,
-				202, 180, 163, 142, 123, 114, 106, 94, 84, 64, 26, 27 };
-		for (int i = 0; i < r.length; i++)
-		{
-			reds[i] = (byte) r[i];
-			greens[i] = (byte) g[i];
-			blues[i] = (byte) b[i];
-		}
-		return r.length;
-	}
-
-	/**
-	 * Adapted from ij.plugin.LutLoader to remove the dark colours
-	 * 
-	 * @param reds
-	 * @param greens
-	 * @param blues
-	 * @return
-	 */
-	private int fire(byte[] reds, byte[] greens, byte[] blues)
-	{
-		int[] r = { //0, 0, 1, 25, 49, 
-		73, 98, 122, 146, 162, 173, 184, 195, 207, 217, 229, 240, 252, 255, 255, 255, 255, 255, 255, 255, 255, 255,
-				255, 255, 255, 255, 255 };
-		int[] g = { //0, 0, 0, 0, 0, 
-		0, 0, 0, 0, 0, 0, 0, 0, 14, 35, 57, 79, 101, 117, 133, 147, 161, 175, 190, 205, 219, 234, 248, 255, 255, 255,
-				255 };
-		int[] b = { //0, 61, 96, 130, 165, 
-		192, 220, 227, 210, 181, 151, 122, 93, 64, 35, 5, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 35, 98, 160, 223, 255 };
-		for (int i = 0; i < r.length; i++)
-		{
-			reds[i] = (byte) r[i];
-			greens[i] = (byte) g[i];
-			blues[i] = (byte) b[i];
-		}
-		return r.length;
-	}
-
-	/**
-	 * Copied from ij.plugin.LutLoader.
-	 * 
-	 * @param reds
-	 * @param greens
-	 * @param blues
-	 * @param nColors
-	 */
-	private void interpolate(byte[] reds, byte[] greens, byte[] blues, int nColors)
-	{
-		byte[] r = new byte[nColors];
-		byte[] g = new byte[nColors];
-		byte[] b = new byte[nColors];
-		System.arraycopy(reds, 0, r, 0, nColors);
-		System.arraycopy(greens, 0, g, 0, nColors);
-		System.arraycopy(blues, 0, b, 0, nColors);
-		double scale = nColors / 256.0;
-		int i1, i2;
-		double fraction;
-		for (int i = 0; i < 256; i++)
-		{
-			i1 = (int) (i * scale);
-			i2 = i1 + 1;
-			if (i2 == nColors)
-				i2 = nColors - 1;
-			fraction = i * scale - i1;
-			//IJ.write(i+" "+i1+" "+i2+" "+fraction);
-			reds[i] = (byte) ((1.0 - fraction) * (r[i1] & 255) + fraction * (r[i2] & 255));
-			greens[i] = (byte) ((1.0 - fraction) * (g[i1] & 255) + fraction * (g[i2] & 255));
-			blues[i] = (byte) ((1.0 - fraction) * (b[i1] & 255) + fraction * (b[i2] & 255));
-		}
-	}
-
+	
 	private void addToOverlay(Overlay o, Roi roi, boolean isHyperStack, int frame)
 	{
 		if (isHyperStack)
