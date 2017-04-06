b509782c1adce892291ceceb7580c2d4c29c9721
aherbert <a.herbert@sussex.ac.uk>
2017-04-06 12:42:54 +0100

    Changed the plugin to add crosstalk linear unmixing
    Added JUnit test of unmixing

--- src/main/java/gdsc/smlm/ij/plugins/PulseActivationAnalysis.java
+++ src/main/java/gdsc/smlm/ij/plugins/PulseActivationAnalysis.java
@@ -7,6 +7,7 @@ import java.util.ArrayList;
 import gdsc.core.ij.Utils;
 import gdsc.core.utils.Settings;
 import gdsc.core.utils.TurboList;
+import gdsc.smlm.fitting.linear.EJMLLinearSolver;
 
 /*----------------------------------------------------------------------------- 
  * GDSC Plugins for ImageJ
@@ -40,14 +41,20 @@ import ij.gui.GenericDialog;
 import ij.plugin.PlugIn;
 
 /**
- * Count the number of times a molecules is localised immediately after an activation pulse compared to any other
- * activation.
+ * Perform multi-channel super-resolution imaging by means of photo-switchable probes and pulsed light activation.
+ * 
+ * This plugin is based on the methods described in: Mark Bates, Bo Huang, Graham T. Dempsey, Xiaowei Zhuang (2007).
+ * Multicolor Super-Resolution Imaging with Photo-Switchable Fluorescent Probes. Science 317, 1749. DOI:
+ * 10.1126/science.1146598.
  */
 public class PulseActivationAnalysis implements PlugIn, DialogListener
 {
-	private static final String TITLE = "Pulse Activation Analysis";
+	private String TITLE = " Activation Analysis";
 
 	private static String inputOption = "";
+	private static int channels = 1;
+	private static final int MAX_CHANNELS = 3;
+
 	private static int pulseInterval = 10;
 	private static int afterPulseStart = 1;
 	private static int afterPulseEnd = 1;
@@ -98,7 +105,9 @@ public class PulseActivationAnalysis implements PlugIn, DialogListener
 			return;
 		}
 
-		if (!showDialog())
+		boolean crosstalkMode = "crosstalk".equals(arg);
+
+		if (!showDialog(crosstalkMode))
 			return;
 
 		// Load the results
@@ -125,16 +134,26 @@ public class PulseActivationAnalysis implements PlugIn, DialogListener
 			this.activations.add(new PulseActivationResult(trace));
 		}
 
-		showAnalysisDialog();
+		if (crosstalkMode)
+			runCrosstalkAnalysis();
+		else
+			runPulseAnalysis();
 	}
 
-	private boolean showDialog()
+	private boolean showDialog(boolean crosstalkMode)
 	{
+		TITLE = ((crosstalkMode) ? "Crosstalk" : "Pulse") + TITLE;
+
 		GenericDialog gd = new GenericDialog(TITLE);
 
-		gd.addMessage("Count & plot molecules activated after a pulse");
+		if (crosstalkMode)
+			gd.addMessage("Analyse crosstalk activation rate");
+		else
+			gd.addMessage("Count & plot molecules activated after a pulse");
+
 		ResultsManager.addInput(gd, "Input", inputOption, InputSource.MEMORY_CLUSTERED);
-		gd.addNumericField("Pulse_interval", pulseInterval, 0);
+		if (!crosstalkMode)
+			gd.addSlider("Channels", 1, MAX_CHANNELS, channels);
 
 		gd.showDialog();
 
@@ -142,18 +161,136 @@ public class PulseActivationAnalysis implements PlugIn, DialogListener
 			return false;
 
 		inputOption = ResultsManager.getInputSource(gd);
-		pulseInterval = (int) Math.abs(gd.getNextNumber());
-
-		if (pulseInterval < 2)
+		if (!crosstalkMode)
 		{
-			IJ.error(TITLE, "Pulse interval must be above 1");
-			return false;
+			channels = (int) gd.getNextNumber();
+
+			if (channels < 1 || channels > MAX_CHANNELS)
+			{
+				IJ.error(TITLE, "Channels must be between 1 and " + MAX_CHANNELS);
+				return false;
+			}
 		}
 
 		return true;
 	}
 
-	private boolean showAnalysisDialog()
+	private void runCrosstalkAnalysis()
+	{
+		// TODO 
+
+		// Determine the cross talk ratio.
+		// This is done by imaging only a single photo-switchable probe with the 
+		// same activation pulse imaging routine used for multi-colour imaging.
+		// Concept:
+		// A probe is meant to turn on in a frame following a pulse from a specific wavelength.
+		// Multi-wavelengths can be used with probes responding to each wavelength. However
+		// each probe may be activated bby the 'wrong' wavelength. This is crosstalk.
+		// The idea is to understand how many times the probe will turn on in a 
+		// frame following a pulse from the other lasers.
+
+		// To determine the crosstalk ratio we must have a single probe imaged with the full
+		// multi-wavelength pulse cycle. We then count how many times a probe activated by 
+		// the correct wavelength is activated by the others.
+		// This is done by grouping localisation into clusters to represent the same molecule.
+		// Crosstalk for each wavelength is then the fraction of times molecules were activated
+		// by the 'wrong' wavelength.
+
+		// Require:
+		// N-channels
+		// The full pulse activation cycle
+		// The target wavelength (i.e. the wavelength that is not 'wrong')
+		// The clustering distance to group activations into molecules
+		// The number of activation to define a molecule
+
+	}
+
+	/**
+	 * Unmix the observed local densities into the actual densities for 2-channels.
+	 * <p>
+	 * Crosstalk from M into N is defined as the number of times the molecule that should be activated by a pulse from
+	 * channel N is activated by a pulse from channel M. A value less than 1 is expected (otherwise the fluorophore is
+	 * being non-specifically activated by is target channel, N).
+	 *
+	 * @param D1
+	 *            the observed density in channel 1
+	 * @param D2
+	 *            the observed density in channel 2
+	 * @param C21
+	 *            the crosstalk from channel 2 into channel 1
+	 * @param C12
+	 *            the crosstalk from channel 1 into channel 2
+	 * @return the actual densities [d1, d2]
+	 */
+	public static double[] unmix(double D1, double D2, double C21, double C12)
+	{
+		double d1, d2;
+		// Solve the equations:
+		// D1 = d1 + C21 * d2
+		// D2 = d2 + C12 * d1
+		// This is done by direct substitution
+		d1 = (D1 - C21 * D2) / (1 - C12 * C21);
+		d2 = D2 - C12 * d1;
+		return new double[] { d1, d2 };
+	}
+
+	/**
+	 * Unmix the observed local densities into the actual densities for 3-channels.
+	 * <p>
+	 * Crosstalk from M into N is defined as the number of times the molecule that should be activated by a pulse from
+	 * channel N is activated by a pulse from channel M. A value less than 1 is expected (otherwise the fluorophore is
+	 * being non-specifically activated by is target channel, N).
+	 *
+	 * @param D1
+	 *            the observed density in channel 1
+	 * @param D2
+	 *            the observed density in channel 2
+	 * @param D3
+	 *            the observed density in channel 3
+	 * @param C21
+	 *            the crosstalk from channel 2 into channel 1
+	 * @param C31
+	 *            the crosstalk from channel 3 into channel 1
+	 * @param C12
+	 *            the crosstalk from channel 1 into channel 2
+	 * @param C32
+	 *            the crosstalk from channel 3 into channel 2
+	 * @param C13
+	 *            the crosstalk from channel 1 into channel 3
+	 * @param C23
+	 *            the crosstalk from channel 2 into channel 3
+	 * @return the actual densities [d1, d2, d3]
+	 */
+	public static double[] unmix(double D1, double D2, double D3, double C21, double C31, double C12, double C32,
+			double C13, double C23)
+	{
+		// Solve the linear equations
+		// D1 = d1 + C21 * d2 + C31 * d3
+		// D2 = d2 + C12 * d1 + C32 * d3
+		// D3 = d3 + C13 * d1 + C23 * d2
+		// This is done using matrix decomposition
+
+		EJMLLinearSolver solver = new EJMLLinearSolver();
+		// @formatter:off
+		double[][] a = { 
+				{ 1, C21, C31 },
+				{ C12, 1, C32 },
+				{ C13, C23, 1 }
+		};
+		// @formatter:on
+		double[] b = { D1, D2, D3 };
+		if (!solver.solveLinear(a, b))
+		{
+			// Unmix failed so reset
+			b[0] = D1;
+			b[1] = D2;
+			b[2] = D3;
+		}
+
+		return b;
+	}
+
+	private boolean runPulseAnalysis()
 	{
 		GenericDialog gd = new GenericDialog(TITLE);
 
@@ -278,8 +415,8 @@ public class PulseActivationAnalysis implements PlugIn, DialogListener
 
 		output.end();
 
-		IJ.showStatus(String.format("%d/%s, %d/%s", count, Utils.pleural(traces.length, "Trace"),
-				output.size(), Utils.pleural(results.size(), "Result")));
+		IJ.showStatus(String.format("%d/%s, %d/%s", count, Utils.pleural(traces.length, "Trace"), output.size(),
+				Utils.pleural(results.size(), "Result")));
 	}
 
 	private Settings lastSettings;
