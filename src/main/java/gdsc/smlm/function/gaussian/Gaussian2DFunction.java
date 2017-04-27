3e4c2f0f6fac73c372ef3cacd58363277a7cf711
aherbert <a.herbert@sussex.ac.uk>
2017-04-27 18:26:17 +0100

    Implement ExtendedNonLinearFunction

--- src/main/java/gdsc/smlm/function/gaussian/Gaussian2DFunction.java
+++ src/main/java/gdsc/smlm/function/gaussian/Gaussian2DFunction.java
@@ -1,7 +1,9 @@
 package gdsc.smlm.function.gaussian;
 
+import org.apache.commons.math3.util.Pair;
+
+import gdsc.smlm.function.ExtendedNonLinearFunction;
 import gdsc.smlm.function.NoiseModel;
-import gdsc.smlm.function.NonLinearFunction;
 
 /*----------------------------------------------------------------------------- 
  * GDSC SMLM Software
@@ -25,7 +27,7 @@ import gdsc.smlm.function.NonLinearFunction;
  * <p>
  * The class provides an index of the position in the parameter array where the parameter is expected.
  */
-public abstract class Gaussian2DFunction implements NonLinearFunction
+public abstract class Gaussian2DFunction implements ExtendedNonLinearFunction
 {
 	/**
 	 * The factor for converting a Gaussian standard deviation to Full Width at Half Maxima (FWHM)
@@ -278,5 +280,65 @@ public abstract class Gaussian2DFunction implements NonLinearFunction
 			if (gradientIndices[i] == parameterIndex)
 				return i;
 		return -1;
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see gdsc.smlm.function.ExtendedNonLinearFunction#computeValues(double[])
+	 */
+	public double[] computeValues(double[] variables)
+	{
+		initialise(variables);
+		final double[] values = new double[maxx * maxy];
+		for (int i = 0; i < values.length; i++)
+		{
+			values[i] = eval(i);
+		}
+		return values;
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see gdsc.smlm.function.ExtendedNonLinearFunction#computeJacobian(double[])
+	 */
+	public double[][] computeJacobian(double[] variables)
+	{
+		return computeValuesAndJacobian(variables).getSecond();
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see gdsc.smlm.function.ExtendedNonLinearFunction#canComputeValuesAndJacobian()
+	 */
+	public boolean canComputeValuesAndJacobian()
+	{
+		return true;
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see gdsc.smlm.function.ExtendedNonLinearFunction#computeValuesAndJacobian(double[])
+	 */
+	public Pair<double[], double[][]> computeValuesAndJacobian(double[] variables)
+	{
+		initialise(variables);
+
+		final int n = maxx * maxy;
+		final double[][] jacobian = new double[n][];
+		final double[] values = new double[n];
+
+		for (int i = 0; i < n; ++i)
+		{
+			// Assume linear X from 0..N
+			final double[] dyda = new double[variables.length];
+			values[i] = eval(i, dyda);
+			jacobian[i] = dyda;
+		}
+
+		return new Pair<double[], double[][]>(values, jacobian);
 	}
 }
