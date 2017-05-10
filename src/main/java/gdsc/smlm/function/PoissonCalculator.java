9cebd5abe30ab8dba60592d0d65e955e0f029318
aherbert <a.herbert@sussex.ac.uk>
2017-05-10 13:35:47 +0100

    Create a Poisson calculator for likelihood computation

---
+++ src/main/java/gdsc/smlm/function/PoissonCalculator.java
@@ -0,0 +1,202 @@
+package gdsc.smlm.function;
+
+import org.apache.commons.math3.distribution.GammaDistribution;
+import org.apache.commons.math3.special.Gamma;
+import org.apache.commons.math3.util.FastMath;
+
+/*----------------------------------------------------------------------------- 
+ * GDSC SMLM Software
+ * 
+ * Copyright (C) 2017 Alex Herbert
+ * Genome Damage and Stability Centre
+ * University of Sussex, UK
+ * 
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or
+ * (at your option) any later version.
+ *---------------------------------------------------------------------------*/
+
+/**
+ * Computes likelihood values for a Poisson function
+ */
+public class PoissonCalculator
+{
+	/**
+	 * Get the Poisson log likelihood of value x given the mean. The mean must be strictly positive. x must be positive.
+	 *
+	 * @param u
+	 *            the mean
+	 * @param x
+	 *            the x
+	 * @return the log likelihood
+	 */
+	public static double logLikelihood(double u, double x)
+	{
+		// Unlikely so skip this ...
+		//if (x == 0)
+		//	return -u;
+		return x * Math.log(u) - u - logFactorial(x);
+	}
+
+	private static double logFactorial(double k)
+	{
+		if (k <= 1)
+			return 0;
+		return Gamma.logGamma(k + 1);
+	}
+
+	/**
+	 * Get the Poisson log likelihood of value x given the mean. The mean must be strictly positive. x must be positive.
+	 *
+	 * @param u
+	 *            the mean
+	 * @param x
+	 *            the x
+	 * @return the log likelihood
+	 */
+	public static double logLikelihood(double[] u, double[] x)
+	{
+		double ll = 0;
+		for (int i = u.length; i-- > 0;)
+			ll += logLikelihood(u[i], x[i]);
+		return ll;
+	}
+
+	/**
+	 * Get the Poisson likelihood of value x given the mean. The mean must be strictly positive. x must be positive.
+	 *
+	 * @param u
+	 *            the mean
+	 * @param x
+	 *            the x
+	 * @return the likelihood
+	 */
+	public static double likelihood(double u, double x)
+	{
+		//return Math.pow(u, x) * FastMath.exp(-u) / factorial(x);
+		return FastMath.exp(logLikelihood(u, x));
+	}
+
+	/**
+	 * Get the Poisson likelihood of value x given the mean. The mean must be strictly positive. x must be positive.
+	 *
+	 * @param u
+	 *            the mean
+	 * @param x
+	 *            the x
+	 * @return the likelihood
+	 */
+	public static double likelihood(double[] u, double[] x)
+	{
+		return FastMath.exp(logLikelihood(u, x));
+	}
+
+	/**
+	 * Get the Poisson maximum log likelihood of value x given the mean is value x. x must be positive.
+	 *
+	 * @param x
+	 *            the x
+	 * @return the log likelihood
+	 */
+	public static double maximumLogLikelihood(double x)
+	{
+		return (x > 0) ? logLikelihood(x, x) : 0;
+	}
+
+	/**
+	 * Get the Poisson maximum log likelihood of value x given the mean is value x. x must be positive.
+	 *
+	 * @param x
+	 *            the x
+	 * @return the log likelihood
+	 */
+	public static double maximumLogLikelihood(double[] x)
+	{
+		double ll = 0;
+		for (int i = x.length; i-- > 0;)
+			ll += maximumLogLikelihood(x[i]);
+		return ll;
+	}
+
+	/**
+	 * Get the Poisson maximum likelihood of value x given the mean is value x. x must be positive.
+	 *
+	 * @param x
+	 *            the x
+	 * @return the likelihood
+	 */
+	public static double maximumLikelihood(double x)
+	{
+		return (x > 0) ? likelihood(x, x) : 1;
+	}
+
+	/**
+	 * Get the Poisson maximum likelihood of value x given the mean is value x. x must be positive.
+	 *
+	 * @param x
+	 *            the x
+	 * @return the likelihood
+	 */
+	public static double maximumLikelihood(double[] x)
+	{
+		return FastMath.exp(maximumLogLikelihood(x));
+	}
+
+	/**
+	 * Get the Poisson log likelihood ratio of value x given the mean. The mean must be strictly positive. x must be
+	 * positive.
+	 *
+	 * @param u
+	 *            the mean
+	 * @param x
+	 *            the x
+	 * @return the log likelihood ratio
+	 */
+	public static double logLikelihoodRatio(double[] u, double[] x)
+	{
+		// From https://en.wikipedia.org/wiki/Likelihood-ratio_test#Use:
+		// LLR = -2 * [ ln(likelihood for alternative model) - ln(likelihood for null model)]
+		// The model with more parameters (here alternative) will always fit at least as well—
+		// i.e., have the same or greater log-likelihood—than the model with fewer parameters 
+		// (here null)
+
+		double ll = 0;
+		for (int i = u.length; i-- > 0;)
+		{
+			//ll += logLikelihood(u[i], x[i]) - maximumLogLikelihood(x[i]);
+
+			if (x[i] > 0)
+			{
+				//ll += (x[i] * Math.log(u[i]) - u[i]) - (x[i] * Math.log(x[i]) - x[i]);
+				//ll += x[i] * Math.log(u[i]) - u[i] - x[i] * Math.log(x[i]) + x[i];
+				//ll += x[i] * (Math.log(u[i]) - Math.log(x[i])) - u[i] + x[i];
+				ll += x[i] * Math.log(u[i] / x[i]) - u[i] + x[i];
+			}
+			else
+			{
+				ll -= u[i];
+			}
+		}
+		return -2 * ll;
+	}
+
+	/**
+	 * Compute the p-value of the log-likelihood ratio using Wilk's theorem that the ratio asymptotically approaches a
+	 * Chi-squared distribution with degrees of freedom equal to the difference in dimensionality of the two models
+	 * (alternative and null).
+	 *
+	 * @param logLikelihoodRatio
+	 *            the log-likelihood ratio
+	 * @param degreesOfFreedom
+	 *            the degrees of freedom
+	 * @return the p-value
+	 * @see https://en.wikipedia.org/wiki/Likelihood-ratio_test#Wilks.27_theorem
+	 */
+	public static double computePValue(double logLikelihoodRatio, int degreesOfFreedom)
+	{
+		// The ChiSquaredDistribution just wraps the gamma distribution for this function
+		//return new ChiSquaredDistribution(degreesOfFreedom).cumulativeProbability(llr);
+		return new GammaDistribution(degreesOfFreedom / 2, 2).cumulativeProbability(logLikelihoodRatio);
+	}
+}
---
+++ src/test/java/gdsc/smlm/function/PoissonCalculatorTest.java
@@ -0,0 +1,215 @@
+package gdsc.smlm.function;
+
+import java.math.BigDecimal;
+import java.math.MathContext;
+
+import org.apache.commons.math3.analysis.UnivariateFunction;
+import org.apache.commons.math3.analysis.integration.SimpsonIntegrator;
+import org.apache.commons.math3.analysis.integration.UnivariateIntegrator;
+import org.apache.commons.math3.distribution.PoissonDistribution;
+import org.apache.commons.math3.random.RandomDataGenerator;
+import org.apache.commons.math3.random.Well19937c;
+import org.apache.commons.math3.util.FastMath;
+import org.junit.Assert;
+import org.junit.Test;
+
+import gdsc.core.ij.Utils;
+
+public class PoissonCalculatorTest
+{
+	double[] photons = { 1, 1.5, 2, 2.5, 3, 4, 5, 7.5, 10, 100, 1000 };
+	private int maxx = 10;
+
+	double P_LIMIT = 0.999999;
+
+	@Test
+	public void canCopmuteLikelihoodForIntegerData()
+	{
+		for (double u : photons)
+		{
+			PoissonDistribution pd = new PoissonDistribution(u);
+			for (int x = 0; x < 100; x++)
+			{
+				double e = pd.probability(x);
+				double o = PoissonCalculator.likelihood(u, x);
+				if (e > 1e-100)
+					Assert.assertEquals(e, o, e * 1e-10);
+				e = pd.logProbability(x);
+				o = PoissonCalculator.logLikelihood(u, x);
+				Assert.assertEquals(e, o, Math.abs(e) * 1e-10);
+			}
+		}
+	}
+
+	@Test
+	public void cumulativeProbabilityIsOneWithRealDataForCountAbove8()
+	{
+		for (double mu : photons)
+		{
+			// Determine upper limit for a Poisson
+			double max = new PoissonDistribution(mu).inverseCumulativeProbability(P_LIMIT);
+
+			// Determine lower limit
+			double sd = Math.sqrt(mu);
+			double min = (int) Math.max(0, mu - 4 * sd);
+
+			cumulativeProbabilityIsOneWithRealData(mu, min, max, mu > 8);
+		}
+	}
+
+	private void cumulativeProbabilityIsOneWithRealData(final double mu, double min, double max, boolean test)
+	{
+		double p = 0;
+
+		UnivariateIntegrator in = new SimpsonIntegrator();
+
+		p = in.integrate(20000, new UnivariateFunction()
+		{
+			public double value(double x)
+			{
+				double v;
+				v = PoissonCalculator.likelihood(mu, x);
+				//v = pgf.probability(x, mu);
+				//System.out.printf("x=%f, v=%f\n", x, v);
+				return v;
+			}
+		}, min, max);
+
+		//System.out.printf("mu=%f, p=%f\n", mu, p);
+		if (test)
+		{
+			Assert.assertEquals(String.format("mu=%f", mu), P_LIMIT, p, 0.02);
+		}
+	}
+
+	private abstract class BaseNonLinearFunction implements NonLinearFunction
+	{
+		double[] a;
+		String name;
+
+		BaseNonLinearFunction(String name)
+		{
+			this.name = name;
+		}
+
+		public void initialise(double[] a)
+		{
+			this.a = a;
+		}
+
+		public int[] gradientIndices()
+		{
+			return new int[1];
+		}
+
+		public double eval(int x, double[] dyda, double[] w)
+		{
+			return 0;
+		}
+
+		public double eval(int x, double[] dyda)
+		{
+			return 0;
+		}
+
+		public boolean canComputeWeights()
+		{
+			return false;
+		}
+
+		public double evalw(int x, double[] w)
+		{
+			return 0;
+		}
+	}
+
+	@Test
+	public void canComputeLogLikelihoodRatio()
+	{
+		final double n2 = maxx * maxx * 0.5;
+		// Functions must produce a strictly positive output
+		//@formatter:off
+		canComputeLogLikelihoodRatio(new BaseNonLinearFunction("Quadratic")
+		{
+			public double eval(int x) {	return 0.1 + 10 * a[0] * (x-n2) * (x-n2); }
+		});		
+		canComputeLogLikelihoodRatio(new BaseNonLinearFunction("Gaussian")
+		{
+			public double eval(int x) {	return 0.1 + 100 * FastMath.exp(-0.5 * Math.pow(x - n2, 2) / (a[0] * a[0])); }
+		});		
+		//@formatter:on
+	}
+
+	private void canComputeLogLikelihoodRatio(BaseNonLinearFunction nlf)
+	{
+		System.out.println(nlf.name);
+
+		int n = maxx * maxx;
+
+		double[] a = new double[] { 1 };
+
+		// Simulate Poisson process
+		nlf.initialise(a);
+		RandomDataGenerator rdg = new RandomDataGenerator(new Well19937c(30051977));
+		double[] x = Utils.newArray(n, 0, 1.0);
+		double[] u = new double[x.length];
+		for (int i = 0; i < n; i++)
+		{
+			u[i] = nlf.eval(i);
+			if (u[i] > 0)
+				x[i] = rdg.nextPoisson(u[i]);
+		}
+
+		double ll = PoissonCalculator.logLikelihood(u, x);
+		double mll = PoissonCalculator.maximumLogLikelihood(x);
+		double llr = -2 * (ll - mll);
+		double llr2 = PoissonCalculator.logLikelihoodRatio(u, x);
+		System.out.printf("llr=%f, llr2=%f\n", llr, llr2);
+		Assert.assertEquals("Log-likelihood ratio", llr, llr2, llr * 1e-10);
+
+		double[] op = new double[x.length];
+		for (int i = 0; i < n; i++)
+			op[i] = PoissonCalculator.maximumLikelihood(x[i]);
+
+		double max = Double.NEGATIVE_INFINITY;
+		double maxa = 0;
+		for (int i = 5; i <= 15; i++)
+		{
+			a[0] = (double) i / 10;
+			nlf.initialise(a);
+			for (int j = 0; j < n; j++)
+				u[j] = nlf.eval(j);
+
+			ll = PoissonCalculator.logLikelihood(u, x);
+			llr = PoissonCalculator.logLikelihoodRatio(u, x);
+			BigDecimal product = new BigDecimal(1);
+			double ll2 = 0;
+			for (int j = 0; j < n; j++)
+			{
+				double p1 = PoissonCalculator.likelihood(u[j], x[j]);
+				ll2 += Math.log(p1);
+				double ratio = p1 / op[j];
+				product = product.multiply(new BigDecimal(ratio));
+			}
+			llr2 = -2 * Math.log(product.doubleValue());
+			double p = 1 - PoissonCalculator.computePValue(llr, n - 1);
+			System.out.printf("a=%f, ll=%f, ll2=%f, llr=%f, llr2=%f, product=%s, p=%f\n", a[0], ll, ll2, llr, llr2,
+					product.round(new MathContext(4)).toString(), p);
+			if (max < ll)
+			{
+				max = ll;
+				maxa = a[0];
+			}
+
+			// Only value if the product could be computed. Low ratios cause it to becomes 
+			// too small to store in a double.
+			if (product.doubleValue() > 0)
+			{
+				Assert.assertEquals("Log-likelihood", ll, ll2, Math.abs(ll2) * 1e-10);
+				Assert.assertEquals("Log-likelihood ratio", llr, llr2, Math.abs(llr) * 1e-10);
+			}
+		}
+
+		Assert.assertEquals("max", 1, maxa, 0);
+	}
+}
