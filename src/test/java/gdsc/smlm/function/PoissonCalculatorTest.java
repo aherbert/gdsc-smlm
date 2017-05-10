d0305ffc328428752391aa80c526f1babc950a20
aherbert <a.herbert@sussex.ac.uk>
2017-05-10 20:00:05 +0100

    cannotSubtractConstantBackgroundAndComputeLogLikelihoodRatio

--- src/test/java/gdsc/smlm/function/PoissonCalculatorTest.java
+++ src/test/java/gdsc/smlm/function/PoissonCalculatorTest.java
@@ -216,4 +216,112 @@ public class PoissonCalculatorTest
 
 		Assert.assertEquals("max", 1, maxa, 0);
 	}
+
+	@Test
+	public void cannotSubtractConstantBackgroundAndComputeLogLikelihoodRatio()
+	{
+		int n = maxx * maxx;
+		final double n2 = n * 0.5;
+		final double n3 = n * 0.33;
+		final double n4 = n * 0.66;
+		// Functions must produce a strictly positive output so add background
+		//@formatter:off
+		cannotSubtractConstantBackgroundAndComputeLogLikelihoodRatio(
+		new BaseNonLinearFunction("Quadratic")
+		{
+			public double eval(int x) {	return 0.1 + a[0] * (x-n2) * (x-n2); }
+		},
+		new BaseNonLinearFunction("Quadratic")
+		{
+			public double eval(int x) {	return 0.2 + 0.5 * a[0] * (x-n3) * (x-n3); }
+		},
+		new BaseNonLinearFunction("Quadratic")
+		{
+			public double eval(int x) {	return 0.3 + 0.75 * a[0] * (x-n4) * (x-n4); }
+		});		
+		cannotSubtractConstantBackgroundAndComputeLogLikelihoodRatio(
+		new BaseNonLinearFunction("Gaussian")
+		{
+			public double eval(int x) {	return 0.1 + 100 * FastMath.exp(-0.5 * Math.pow(x - n2, 2) / (a[0] * a[0])); }
+		},
+		new BaseNonLinearFunction("Gaussian")
+		{
+			public double eval(int x) {	return 0.2 + 50 * FastMath.exp(-0.5 * Math.pow(x - n3, 2) / (a[0] * a[0])); }
+		},
+		new BaseNonLinearFunction("Gaussian")
+		{
+			public double eval(int x) {	return 0.3 + 75 * FastMath.exp(-0.5 * Math.pow(x - n4, 2) / (a[0] * a[0])); }
+		});		
+		//@formatter:on
+	}
+
+	private void cannotSubtractConstantBackgroundAndComputeLogLikelihoodRatio(BaseNonLinearFunction nlf1,
+			BaseNonLinearFunction nlf2, BaseNonLinearFunction nlf3)
+	{
+		System.out.println(nlf1.name);
+
+		int n = maxx * maxx;
+
+		double[] a = new double[] { 1 };
+
+		// Simulate Poisson process of 3 combined functions
+		nlf1.initialise(a);
+		nlf2.initialise(a);
+		nlf3.initialise(a);
+		RandomDataGenerator rdg = new RandomDataGenerator(new Well19937c(30051977));
+		double[] x = Utils.newArray(n, 0, 1.0);
+		double[] u = new double[x.length];
+		double[] b1 = new double[x.length];
+		double[] b2 = new double[x.length];
+		double[] b3 = new double[x.length];
+		for (int i = 0; i < n; i++)
+		{
+			b1[i] = nlf1.eval(i);
+			b2[i] = nlf2.eval(i);
+			b3[i] = nlf3.eval(i);
+			u[i] = b1[i] + b2[i] + b3[i];
+			if (u[i] > 0)
+				x[i] = rdg.nextPoisson(u[i]);
+		}
+
+		// x is the target data
+		// b1 is the already computed background
+		// b2 is the first function to add to the model
+		// b3 is the second function to add to the model
+
+		// Compute the LLR of adding b3 to b2 given we already have b1 modelling data x
+		double[] b12 = add(b1, b2);
+		double ll1a = PoissonCalculator.logLikelihood(b12, x);
+		double ll2a = PoissonCalculator.logLikelihood(add(b12, b3), x);
+		double llra = -2 * (ll1a - ll2a);
+		System.out.printf("x|(a+b+c) ll1=%f, ll2=%f, llra=%f\n", ll1a, ll2a, llra);
+
+		// Compute the LLR of adding b3 to b2 given we already have x minus b1
+		x = subtract(x, b1);
+		double ll1b = PoissonCalculator.logLikelihood(b2, x);
+		double ll2b = PoissonCalculator.logLikelihood(add(b2, b3), x);
+		double llrb = -2 * (ll1b - ll2b);
+		System.out.printf("x-a|(b+c) : ll1=%f, ll2=%f, llrb=%f\n", ll1b, ll2b, llrb);
+
+		System.out.printf("llr=%f (%g), llr2=%f (%g)\n", llra, PoissonCalculator.computePValue(llra, 1), llrb,
+				PoissonCalculator.computePValue(llrb, 1));
+		Assert.assertNotEquals("Log-likelihood ratio", llra, llrb, llra * 1e-10);
+
+	}
+
+	private double[] add(double[] a, double[] b)
+	{
+		double[] c = new double[a.length];
+		for (int i = 0; i < a.length; i++)
+			c[i] = a[i] + b[i];
+		return c;
+	}
+
+	private double[] subtract(double[] a, double[] b)
+	{
+		double[] c = new double[a.length];
+		for (int i = 0; i < a.length; i++)
+			c[i] = a[i] - b[i];
+		return c;
+	}
 }
