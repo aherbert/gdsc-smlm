c117005d3471c2b84dac20f88374f5e3aef5e83f
aherbert <a.herbert@sussex.ac.uk>
2017-05-15 11:22:35 +0100

    New weighted least square function solver

---
+++ src/main/java/gdsc/smlm/fitting/WLSEFunctionSolver.java
@@ -0,0 +1,34 @@
+package gdsc.smlm.fitting;
+
+/*----------------------------------------------------------------------------- 
+ * GDSC SMLM Software
+ * 
+ * Copyright (C) 2017 Alex Herbert
+ * Genome Damage and Stability Centre
+ * University of Sussex, UK
+ * 
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or
+ * (at your option) any later version.
+ *---------------------------------------------------------------------------*/
+
+/**
+ * Defines methods to fit a function with coefficients (a) using weighted least-squares estimation.
+ */
+public interface WLSEFunctionSolver extends FunctionSolver 
+{
+	/**
+	 * Gets the chi squared.
+	 *
+	 * @return the chi squared
+	 */
+	public double getChiSquared();
+	
+	/**
+	 * Gets the probability Q that a value of chi-square as poor as the value should occur by chance.
+	 *
+	 * @return the q-value
+	 */
+	public double getQ();
+}
