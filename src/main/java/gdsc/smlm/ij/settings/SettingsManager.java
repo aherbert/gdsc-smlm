9238069dd974e85e625234c3b5e508d1e39e0730
Alex Herbert <a.herbert@sussex.ac.uk>
2017-07-07 12:35:01 +0100

    Removed GlobalSettings

--- src/main/java/gdsc/smlm/ij/settings/SettingsManager.java
+++ src/main/java/gdsc/smlm/ij/settings/SettingsManager.java
@@ -18,24 +18,37 @@ import com.google.protobuf.Message;
 import com.google.protobuf.Parser;
 import com.google.protobuf.util.JsonFormat;
 import com.google.protobuf.util.JsonFormat.Printer;
-import com.thoughtworks.xstream.XStream;
-import com.thoughtworks.xstream.XStreamException;
-import com.thoughtworks.xstream.io.xml.DomDriver;
 
 import gdsc.core.clustering.ClusteringAlgorithm;
 import gdsc.core.utils.BitFlags;
 import gdsc.smlm.data.NamedObject;
 import gdsc.smlm.data.config.CalibrationConfig.Calibration;
 import gdsc.smlm.data.config.CalibrationConfigHelper;
-import gdsc.smlm.data.config.FitConfig.*;
+import gdsc.smlm.data.config.FitConfig.DataFilterMethod;
+import gdsc.smlm.data.config.FitConfig.DataFilterType;
+import gdsc.smlm.data.config.FitConfig.FitEngineSettings;
+import gdsc.smlm.data.config.FitConfig.FitSolver;
+import gdsc.smlm.data.config.FitConfig.NoiseEstimatorMethod;
 import gdsc.smlm.data.config.FitConfigHelper;
-import gdsc.smlm.data.config.GUIConfig.*;
+import gdsc.smlm.data.config.GUIConfig.ClusteringSettings;
+import gdsc.smlm.data.config.GUIConfig.CreateDataSettings;
+import gdsc.smlm.data.config.GUIConfig.GUIFilterSettings;
+import gdsc.smlm.data.config.GUIConfig.LoadLocalisationsSettings;
+import gdsc.smlm.data.config.GUIConfig.OpticsSettings;
+import gdsc.smlm.data.config.GUIConfig.PSFCalculatorSettings;
+import gdsc.smlm.data.config.GUIConfig.PSFEstimatorSettings;
 import gdsc.smlm.data.config.GUIConfigHelper;
-import gdsc.smlm.data.config.ResultsConfig.*;
+import gdsc.smlm.data.config.ResultsConfig.ResultsFileFormat;
+import gdsc.smlm.data.config.ResultsConfig.ResultsImageType;
+import gdsc.smlm.data.config.ResultsConfig.ResultsSettings;
 import gdsc.smlm.data.config.ResultsConfigHelper;
-import gdsc.smlm.data.config.UnitConfig.*;
+import gdsc.smlm.data.config.UnitConfig.AngleUnit;
+import gdsc.smlm.data.config.UnitConfig.DistanceUnit;
+import gdsc.smlm.data.config.UnitConfig.IntensityUnit;
+import gdsc.smlm.data.config.UnitConfig.TimeUnit;
 import gdsc.smlm.data.config.UnitHelper;
-import gdsc.smlm.engine.FitConfiguration;
+import ij.IJ;
+import ij.Prefs;
 
 /*----------------------------------------------------------------------------- 
  * GDSC SMLM Software
@@ -49,10 +62,6 @@ import gdsc.smlm.engine.FitConfiguration;
  * the Free Software Foundation; either version 3 of the License, or
  * (at your option) any later version.
  *---------------------------------------------------------------------------*/
-
-import gdsc.smlm.engine.FitEngineConfiguration;
-import ij.IJ;
-import ij.Prefs;
 
 /**
  * Manage the settings for ImageJ plugins
@@ -110,8 +119,6 @@ public class SettingsManager
 			IJ.log("Unable create settings directory: " + e.getMessage());
 		}
 	}
-
-	private static XStream xs = null;
 
 	// Encapsulate the values and names of enums for lazy loading
 
@@ -506,372 +513,6 @@ public class SettingsManager
 	}
 
 	/**
-	 * Save the configuration to file
-	 * 
-	 * @param config
-	 * @param filename
-	 * @return True if saved
-	 */
-	public static boolean saveFitEngineConfiguration(FitEngineConfiguration config, String filename)
-	{
-		XStream xs = createXStream();
-		FileOutputStream fs = null;
-		try
-		{
-			fs = new FileOutputStream(filename);
-			xs.toXML(config, fs);
-			return true;
-		}
-		catch (FileNotFoundException ex)
-		{
-			//ex.printStackTrace();
-		}
-		catch (XStreamException ex)
-		{
-			ex.printStackTrace();
-		}
-		finally
-		{
-			if (fs != null)
-			{
-				try
-				{
-					fs.close();
-				}
-				catch (IOException e)
-				{
-					e.printStackTrace();
-				}
-			}
-		}
-		return false;
-	}
-
-	private static XStream createXStream()
-	{
-		if (xs == null)
-		{
-			xs = new XStream(new DomDriver());
-
-			// Map the object names/fields for a nicer configuration file
-			xs.alias("gdsc.fitting.settings", GlobalSettings.class);
-			xs.alias("gdsc.fitting.configuration", FitEngineConfiguration.class);
-
-			xs.aliasField("gdsc.fitting.configuration", GlobalSettings.class, "fitEngineConfiguration");
-
-			xs.aliasField("peakParameters", FitEngineConfiguration.class, "fitConfiguration");
-			xs.aliasField("smoothing", FitEngineConfiguration.class, "smooth");
-			//xs.aliasField("width0", FitConfiguration.class, "initialPeakWidth0");
-			//xs.aliasField("width1", FitConfiguration.class, "initialPeakWidth1");
-			//xs.aliasField("angle", FitConfiguration.class, "initialAngle");
-			xs.aliasField("enableValidation", FitConfiguration.class, "fitValidation");
-
-			xs.omitField(FitConfiguration.class, "flags");
-			xs.omitField(FitConfiguration.class, "signalThreshold");
-			//xs.omitField(FitConfiguration.class, "noise");
-			xs.omitField(FitConfiguration.class, "enableValidation");
-			xs.omitField(FitConfiguration.class, "computeResiduals");
-
-			// Smart filter settings
-			xs.omitField(FitConfiguration.class, "directFilter");
-			xs.omitField(FitConfiguration.class, "dynamicPeakResult");
-			xs.omitField(FitConfiguration.class, "filterResult");
-			xs.omitField(FitConfiguration.class, "widthEnabled");
-			xs.omitField(FitConfiguration.class, "offset");
-		}
-
-		return xs;
-	}
-
-	/**
-	 * Load the configuration within the specified file
-	 * 
-	 * @param filename
-	 * @return The configuration (or null)
-	 */
-	public static FitEngineConfiguration unsafeLoadFitEngineConfiguration(String filename)
-	{
-		XStream xs = createXStream();
-		FitEngineConfiguration config = null;
-
-		FileInputStream fs = null;
-		try
-		{
-			fs = new FileInputStream(filename);
-			config = (FitEngineConfiguration) xs.fromXML(fs);
-		}
-		catch (ClassCastException ex)
-		{
-			//ex.printStackTrace();
-		}
-		catch (FileNotFoundException ex)
-		{
-			//ex.printStackTrace();
-		}
-		catch (XStreamException ex)
-		{
-			ex.printStackTrace();
-		}
-		finally
-		{
-			if (fs != null)
-			{
-				try
-				{
-					fs.close();
-				}
-				catch (IOException e)
-				{
-					e.printStackTrace();
-				}
-			}
-		}
-
-		return config;
-	}
-
-	/**
-	 * Load the configuration within the specified file
-	 * 
-	 * @param filename
-	 * @return The configuration (or a default instance)
-	 */
-	public static FitEngineConfiguration loadFitEngineConfiguration(String filename)
-	{
-		FitEngineConfiguration config = unsafeLoadFitEngineConfiguration(filename);
-		if (config == null)
-			config = new FitEngineConfiguration();
-		return config;
-	}
-
-	/**
-	 * Save the settings to file.
-	 * <p>
-	 * If this fails then an error message is written to the ImageJ log
-	 *
-	 * @param settings
-	 *            the settings
-	 * @param filename
-	 *            the filename
-	 * @return True if saved
-	 */
-	public static boolean saveSettings(GlobalSettings settings, String filename)
-	{
-		return saveSettings(settings, filename, false);
-	}
-
-	/**
-	 * Save the settings to file.
-	 * <p>
-	 * If this fails then an error message is written to the ImageJ log
-	 *
-	 * @param settings
-	 *            the settings
-	 * @param filename
-	 *            the filename
-	 * @param silent
-	 *            Set to true to suppress writing an error message to the ImageJ log
-	 * @return True if saved
-	 */
-	public static boolean saveSettings(GlobalSettings settings, String filename, boolean silent)
-	{
-		XStream xs = createXStream();
-		FileOutputStream fs = null;
-		try
-		{
-			fs = new FileOutputStream(filename);
-			xs.toXML(settings, fs);
-			return true;
-		}
-		catch (FileNotFoundException ex)
-		{
-			//ex.printStackTrace();
-		}
-		catch (XStreamException ex)
-		{
-			ex.printStackTrace();
-		}
-		finally
-		{
-			if (fs != null)
-			{
-				try
-				{
-					fs.close();
-				}
-				catch (IOException e)
-				{
-					e.printStackTrace();
-				}
-			}
-		}
-		if (!silent)
-			IJ.log("Unable to save settings to: " + filename);
-		return false;
-	}
-
-	/**
-	 * Save the settings to the default file.
-	 * <p>
-	 * If this fails then an error message is written to the ImageJ log
-	 *
-	 * @param settings
-	 *            the settings
-	 * @return True if saved
-	 */
-	public static boolean saveSettings(GlobalSettings settings)
-	{
-		return saveSettings(settings, getSettingsFilename(), false);
-	}
-
-	/**
-	 * Load the settings within the specified file
-	 * <p>
-	 * If this fails then an error message is written to the ImageJ log
-	 * 
-	 * @param filename
-	 * @param silent
-	 *            Set to true to suppress writing an error message to the ImageJ log
-	 * @return The settings (or null)
-	 */
-	public static GlobalSettings unsafeLoadSettings(String filename, boolean silent)
-	{
-		XStream xs = createXStream();
-		GlobalSettings config = null;
-
-		FileInputStream fs = null;
-		try
-		{
-			fs = new FileInputStream(filename);
-			config = (GlobalSettings) xs.fromXML(fs);
-		}
-		catch (ClassCastException ex)
-		{
-			//ex.printStackTrace();
-		}
-		catch (FileNotFoundException ex)
-		{
-			//ex.printStackTrace();
-		}
-		catch (XStreamException ex)
-		{
-			ex.printStackTrace();
-		}
-		finally
-		{
-			if (fs != null)
-			{
-				try
-				{
-					fs.close();
-				}
-				catch (IOException e)
-				{
-					e.printStackTrace();
-				}
-			}
-		}
-
-		if (config == null)
-			if (!silent)
-				IJ.log("Unable to load settings from: " + filename);
-
-		return config;
-	}
-
-	/**
-	 * Load the settings from the input stream. The stream will be closed.
-	 * <p>
-	 * If this fails then an error message is written to the ImageJ log
-	 *
-	 * @param inputStream
-	 *            the input stream
-	 * @param silent
-	 *            Set to true to suppress writing an error message to the ImageJ log
-	 * @return The settings (or null)
-	 */
-	public static GlobalSettings unsafeLoadSettings(InputStream inputStream, boolean silent)
-	{
-		XStream xs = createXStream();
-		GlobalSettings config = null;
-
-		try
-		{
-			config = (GlobalSettings) xs.fromXML(inputStream);
-		}
-		catch (ClassCastException ex)
-		{
-			//ex.printStackTrace();
-		}
-		catch (XStreamException ex)
-		{
-			ex.printStackTrace();
-		}
-		finally
-		{
-			try
-			{
-				inputStream.close();
-			}
-			catch (IOException e)
-			{
-				e.printStackTrace();
-			}
-		}
-
-		if (config == null)
-			if (!silent)
-				IJ.log("Unable to load settings from input stream");
-
-		return config;
-	}
-
-	/**
-	 * Load the settings within the specified file
-	 * <p>
-	 * If this fails then an error message is written to the ImageJ log
-	 * 
-	 * @param filename
-	 * @return The settings (or a default instance)
-	 */
-	public static GlobalSettings loadSettings(String filename)
-	{
-		return loadSettings(filename, false);
-	}
-
-	/**
-	 * Load the settings within the specified file
-	 * <p>
-	 * If this fails then an error message is written to the ImageJ log
-	 * 
-	 * @param filename
-	 * @param silent
-	 *            Set to true to suppress writing an error message to the ImageJ log
-	 * @return The settings (or a default instance)
-	 */
-	public static GlobalSettings loadSettings(String filename, boolean silent)
-	{
-		GlobalSettings config = unsafeLoadSettings(filename, silent);
-		if (config == null)
-		{
-			config = new GlobalSettings();
-		}
-		return config;
-	}
-
-	/**
-	 * Load the settings from the default file
-	 * <p>
-	 * If this fails then an error message is written to the ImageJ log
-	 * 
-	 * @return The settings (or a default instance)
-	 */
-	public static GlobalSettings loadSettings()
-	{
-		return loadSettings(getSettingsFilename(), false);
-	}
-
-	/**
 	 * Creates the settings file using the class name to create the filename in the settings directory.
 	 *
 	 * @param clazz
@@ -1031,7 +672,7 @@ public class SettingsManager
 	{
 		return new ConfigurationReader<GUIFilterSettings>(GUIConfigHelper.defaultGUIFilterSettings).read(flags);
 	}
-	
+
 	/**
 	 * Read the PSFCalculatorSettings from the settings file in the settings directory.
 	 *
@@ -1077,7 +718,8 @@ public class SettingsManager
 	 */
 	public static LoadLocalisationsSettings readLoadLocalisationsSettings(int flags)
 	{
-		return new ConfigurationReader<LoadLocalisationsSettings>(GUIConfigHelper.defaultLoadLocalisationsSettings).read(flags);
+		return new ConfigurationReader<LoadLocalisationsSettings>(GUIConfigHelper.defaultLoadLocalisationsSettings)
+				.read(flags);
 	}
 
 	/**
@@ -1091,7 +733,7 @@ public class SettingsManager
 	{
 		return new ConfigurationReader<ClusteringSettings>(GUIConfigHelper.defaultClusteringSettings).read(flags);
 	}
-	
+
 	/**
 	 * Read the OpticsSettings from the settings file in the settings directory.
 	 *
@@ -1103,7 +745,7 @@ public class SettingsManager
 	{
 		return new ConfigurationReader<OpticsSettings>(GUIConfigHelper.defaultOpticsSettings).read(flags);
 	}
-	
+
 	/**
 	 * Write the message to file.
 	 * <p>
