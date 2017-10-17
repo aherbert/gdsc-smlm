abf32737bc6c6ac874cf11da5cf1adaa244ab5de
ah403 <ah403@gdsc018424.gdsc.susx.ac.uk>
2017-10-17 23:19:27 +0100

    Updated spline data format

--- src/main/java/gdsc/smlm/function/cspline/CubicSplineFunction.java
+++ src/main/java/gdsc/smlm/function/cspline/CubicSplineFunction.java
@@ -2,9 +2,7 @@ package gdsc.smlm.function.cspline;
 
 import java.util.Arrays;
 
-import gdsc.core.math.interpolation.CubicSplinePosition;
 import gdsc.core.math.interpolation.CustomTricubicFunction;
-import gdsc.core.math.interpolation.CustomTricubicInterpolatingFunction;
 import gdsc.core.utils.SimpleArrayUtils;
 import gdsc.smlm.function.Gradient1Procedure;
 import gdsc.smlm.function.Gradient2Function;
@@ -30,33 +28,32 @@ import gdsc.smlm.results.PeakResult;
  */
 public class CubicSplineFunction implements Gradient2Function
 {
-	private class TargetSpline
+	/**
+	 * Internal class to control visiting the correct cubic spline node for each [x][y] index in the
+	 * target range [0 <= x < maxx], [0 <= y < maxy]
+	 */
+	protected abstract class TargetSpline
 	{
-		double tI, tX, tY, tZ;
-		int ix1, ix2, iy1, iy2;
+		int ix1, iy1;
 		int ix0, iy0;
-		int xindex, yindex, zindex;
-		double[] table1 = new double[64];
-		double[] table2;
-		double[] table3;
-		double[] table6;
+		int index;
+		int yindex;
+		CustomTricubicFunction[] xySplines;
 		boolean[] activeX = new boolean[maxx];
 
 		public boolean initialise(double tI, double tX, double tY, double tZ, int order)
 		{
-			this.tI = tI;
-
 			// Map z to a position in the spline
 			// We want 0 to be in the centre. 
 			// Note: Scale up the input parameter to the spline scale.
-			tZ = cz + scale * tZ;
+			double z = cz + scale * tZ;
 
-			if (tZ < 0 || tZ > uz)
+			if (z < 0 || z > maxSz)
 			{
 				return false;
 			}
 
-			// Map the scaled XY spline bounds to the target region (maxx x maxy)
+			// Shift the scaled XY spline bounds by the target centre 
 			double x1 = lx + tX;
 			double x2 = ux + tX;
 			double y1 = ly + tY;
@@ -68,38 +65,85 @@ public class CubicSplineFunction implements Gradient2Function
 				return false;
 			}
 
-			// Convert to integer grid in the target region
-			// We want the first integer that the function overlaps, i.e. can interpolate a value for
+			// Convert the lower bounds to integer grid in the target region, 
+			// i.e. we sample the region at x=0,1,2,...
+			// We want the first integer that the function overlaps, 
+			// i.e. can interpolate a value for so it must be above the lower bounds
 			ix1 = (int) Math.ceil(x1);
 			iy1 = (int) Math.ceil(y1);
-			// We want the last integer that the function overlaps
-			ix2 = (int) Math.floor(x2);
-			iy2 = (int) Math.floor(y2);
 
-			// How far into the unscaled function is the first point
+			// How far into the unscaled function is the first point.
+			// i.e. x=1 may be 0.6 above the scaled lower bound (0.4) but that would require
+			// the first sample to be taken at spline[1] @ 0.2 if the scale is 2.
 			double x = scale * (ix1 - x1);
 			double y = scale * (iy1 - y1);
 
-			// Get the spline index position for 0,0
-			ix0 = (int) x - scale * ix1;
-			iy0 = (int) y - scale * iy1;
-			// Store the z spline node
-			zindex = (int) tZ;
+			// This is the first index for the spline sample
+			int ix = (int) x;
+			int iy = (int) y;
+			int iz = (int) z;
 
-			// Create the interpolation table
-			CubicSplinePosition sz = new CubicSplinePosition(tZ - zindex);
-			CubicSplinePosition sx = new CubicSplinePosition(x - (int) x);
-			CubicSplinePosition sy = new CubicSplinePosition(y - (int) y);
-
-			table1 = CustomTricubicFunction.computePowerTable(sx, sy, sz);
+			// Get the spline index position for 0,0 offset by the scale (for pre-increment loops)
+			ix0 = ix - scale * ix1 - scale;
+			iy0 = iy - scale * iy1 - scale;
+			// Store the xy splines for the z position
+			xySplines = splines[iz];
 
 			// Set the working flag for all x
-			xindex = ix0;
-			for (int i = 0; i < maxx; i++)
+			for (int i = 0, xindex = ix0; i < maxx; i++)
 			{
-				activeX[i] = xindex >= 0 && xindex <= maxSx;
 				xindex += scale;
+				activeX[i] = xindex >= 0 && xindex < maxSx;
 			}
+
+			computePowerTable(x - ix, y - iy, z - iz, order);
+
+			return true;
+		}
+
+		public void reset()
+		{
+			yindex = iy0;
+		}
+
+		public boolean isNextYActive()
+		{
+			// pre-increment yindex
+			yindex += scale;
+			if (yindex >= 0 && yindex < maxSy)
+			{
+				// The y-index is inside the XY spline data
+				// Reset the index
+				index = yindex * maxSx + ix0;
+			}
+			return false;
+		}
+
+		abstract public void computePowerTable(double x, double y, double z, int order);
+
+		public double value(int x)
+		{
+			index += scale; // pre-increment
+			return (activeX[x]) ? computeValue(xySplines[index]) : 0;
+		}
+
+		abstract public double computeValue(CustomTricubicFunction customTricubicFunction);
+	}
+
+	/**
+	 * Double precision computation of the target spline
+	 */
+	protected class DoubleTargetSpline extends TargetSpline
+	{
+		double[] table1 = new double[64];
+		double[] table2;
+		double[] table3;
+		double[] table6;
+
+		@Override
+		public void computePowerTable(double x, double y, double z, int order)
+		{
+			table1 = CustomTricubicFunction.computePowerTable(x, y, z);
 
 			// Create tables for derivatives
 			if (order == 1)
@@ -113,8 +157,6 @@ public class CubicSplineFunction implements Gradient2Function
 				table3 = multiply(3, table3);
 				table6 = multiply(6, table6);
 			}
-
-			return true;
 		}
 
 		private double[] multiply(int n, double[] table)
@@ -126,48 +168,72 @@ public class CubicSplineFunction implements Gradient2Function
 			return table;
 		}
 
-		public boolean isNextYActive()
+		public double computeValue(CustomTricubicFunction customTricubicFunction)
 		{
-			boolean active = yindex >= 0 && yindex <= maxSy;
-			// Increment yindex
-			yindex += scale;
-			// Reset xindex
-			xindex = ix0;
-			return active;
-		}
-
-		public double value(int x)
-		{
-			if (activeX[x])
-			{
-				double v = function.value(xindex, yindex, zindex, table1);
-				xindex += scale;
-				return v;
-			}
-			xindex += scale;
-			return 0;
-		}
-
-		public void reset()
-		{
-			xindex = ix0;
-			yindex = iy0;
+			return customTricubicFunction.value(table1);
 		}
 	}
 
-	/** The cubic spline function. */
-	private final CustomTricubicInterpolatingFunction function;
+	/**
+	 * Double precision computation of the target spline
+	 */
+	protected class FloatTargetSpline extends TargetSpline
+	{
+		float[] table1 = new float[64];
+		float[] table2;
+		float[] table3;
+		float[] table6;
 
-	private final int maxSx, maxSy;
+		@Override
+		public void computePowerTable(double x, double y, double z, int order)
+		{
+			table1 = CustomTricubicFunction.computeFloatPowerTable(x, y, z);
 
-	/** The scale. */
+			// Create tables for derivatives
+			if (order == 1)
+			{
+				table2 = multiply(2, table2);
+				table3 = multiply(3, table3);
+			}
+			else if (order == 2)
+			{
+				table2 = multiply(2, table2);
+				table3 = multiply(3, table3);
+				table6 = multiply(6, table6);
+			}
+		}
+
+		private float[] multiply(int n, float[] table)
+		{
+			if (table == null)
+				table = new float[64];
+			for (int i = 0; i < 64; i++)
+				table[i] = table1[i] * n;
+			return table;
+		}
+
+		public double computeValue(CustomTricubicFunction customTricubicFunction)
+		{
+			return customTricubicFunction.value(table1);
+		}
+	}
+
+	/** The scale to map the target range (maxx * maxy) to the spline. */
 	private int scale = 1;
 
+	// The centre of the spline (unscaled)
 	private double cx, cy, cz;
+
 	// The scaled bounds of the function with the centre at 0,0,0
 	private double lx, ly, ux, uy;
-	private final double uz;
 
+	// Max size of spline data
+	private final int maxSx, maxSy, maxSz;
+
+	// The tricubic spline packed as Z * YX arrays 
+	private final CustomTricubicFunction[][] splines;
+
+	// The target range
 	private final int maxx, maxy;
 
 	private int n = 1;
@@ -187,8 +253,8 @@ public class CubicSplineFunction implements Gradient2Function
 	/**
 	 * Instantiates a new cubic spline function.
 	 *
-	 * @param function
-	 *            the function
+	 * @param splineData
+	 *            the spline data
 	 * @param maxx
 	 *            The maximum x value of the 2-dimensional data
 	 * @param maxy
@@ -196,64 +262,41 @@ public class CubicSplineFunction implements Gradient2Function
 	 * @throws IllegalArgumentException
 	 *             If the function does not have an integer grid spacing from the origin
 	 */
-	public CubicSplineFunction(CustomTricubicInterpolatingFunction function, int maxx, int maxy)
-			throws IllegalArgumentException
+	public CubicSplineFunction(CubicSplineData splineData, int maxx, int maxy) throws IllegalArgumentException
 	{
-		checkFunction(function);
-		this.function = function;
-		maxSx = function.getMaxXSplinePosition();
-		maxSy = function.getMaxYSplinePosition();
+		this.splines = splineData.splines;
 		this.maxx = (maxx < 1) ? 1 : maxx;
 		this.maxy = (maxy < 1) ? 1 : maxy;
+		maxSx = splineData.maxx;
+		maxSy = splineData.maxy;
+		maxSz = splines.length;
 		// Centre in the middle, assuming the min is zero
-		uz = function.getMaxZ();
-		cx = (function.getMaxX() / 2.0);
-		cy = (function.getMaxY() / 2.0);
-		cz = (uz / 2.0);
+		cx = (maxSx / 2.0);
+		cy = (maxSy / 2.0);
+		cz = (maxSz / 2.0);
 		setScale(1);
-	}
-
-	private void checkFunction(CustomTricubicInterpolatingFunction function)
-	{
-		if (function == null || !function.isInteger())
-			throw new IllegalArgumentException("Require integer grid spacing for the cubic spline");
-		if (function.getMinX() != 0 || function.getMinY() != 0 || function.getMinZ() != 0)
-			throw new IllegalArgumentException("Require integer grid spacing at the origin (0,0,0)");
 	}
 
 	/**
 	 * Instantiates a new cubic spline function.
 	 *
-	 * @param function
-	 *            the function
-	 * @param maxx
-	 *            The maximum x value of the 2-dimensional data
-	 * @param maxy
-	 *            The maximum y value of the 2-dimensional data
-	 * @param cx
-	 *            the centre of the cubic spline x dimension
-	 * @param cy
-	 *            the centre of the cubic spline y dimension
-	 * @param cz
-	 *            the centre of the cubic spline z dimension
-	 * @param scale
-	 *            the scale
+	 * @param splineData
+	 *            the spline data
 	 * @throws IllegalArgumentException
 	 *             If the function does not have an integer grid spacing from the origin
 	 */
-	public CubicSplineFunction(CustomTricubicInterpolatingFunction function, int maxx, int maxy, double cx, double cy,
-			double cz, int scale) throws IllegalArgumentException
+	public CubicSplineFunction(CubicSplineData splineData, int maxx, int maxy, double cx, double cy, double cz,
+			int scale) throws IllegalArgumentException
 	{
-		checkFunction(function);
-		this.function = function;
-		maxSx = function.getMaxXSplinePosition();
-		maxSy = function.getMaxYSplinePosition();
+		this.splines = splineData.splines;
 		this.maxx = (maxx < 1) ? 1 : maxx;
 		this.maxy = (maxy < 1) ? 1 : maxy;
+		maxSx = splineData.maxx;
+		maxSy = splineData.maxy;
+		maxSz = splines.length;
 		this.cx = cx;
 		this.cy = cy;
 		this.cz = cz;
-		uz = function.getMaxZ();
 		setScale(scale);
 	}
 
@@ -261,11 +304,11 @@ public class CubicSplineFunction implements Gradient2Function
 	{
 		// Store the bounds of the cubic spline if it were positioned at 0,0
 		lx = -cx / scale;
-		ux = (function.getMaxX() - cx) / scale;
+		ux = (maxSx - cx) / scale;
 		ly = -cy / scale;
-		uy = (function.getMaxY() - cy) / scale;
+		uy = (maxSy - cy) / scale;
 		// Check if the centre was within the function
-		if (lx > 0 || ly > 0 || ux < 0 || uy < 0 || cz < 0 || cz > uz)
+		if (lx > 0 || ly > 0 || ux < 0 || uy < 0 || cz < 0 || cz > maxSz)
 			throw new IllegalArgumentException("Require the centre within the cubic spline");
 	}
 
@@ -390,16 +433,6 @@ public class CubicSplineFunction implements Gradient2Function
 		updateFunctionBounds();
 	}
 
-	/**
-	 * Gets the function.
-	 *
-	 * @return the function
-	 */
-	public CustomTricubicInterpolatingFunction getFunction()
-	{
-		return function;
-	}
-
 	public int size()
 	{
 		return maxx * maxy;
@@ -445,8 +478,9 @@ public class CubicSplineFunction implements Gradient2Function
 		{
 			int m = t.length;
 			t = Arrays.copyOf(t, n); // Preserve memory space
+			boolean sp = splines[0][0].isSinglePrecision();
 			while (m < n)
-				t[m++] = new TargetSpline();
+				t[m++] = (sp) ? new FloatTargetSpline() : new DoubleTargetSpline();
 			working = new TargetSpline[n];
 			workingY = new TargetSpline[n];
 		}
