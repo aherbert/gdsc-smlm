3ae73c5669802a57eb7f5ba0ff6b0e89a0ea5c2e
aherbert <a.herbert@sussex.ac.uk>
2017-11-29 16:52:01 +0000

    Constant for sCMOS analysis dir

--- src/main/java/gdsc/smlm/ij/settings/Constants.java
+++ src/main/java/gdsc/smlm/ij/settings/Constants.java
@@ -60,4 +60,5 @@ public class Constants
 	public static final String inputNoise = "gdsc.smlm.noise";
 	
 	public static final String tiffSeriesDirectory = "gdsc.smlm.tiffSeriesDirectory";
+	public static final String sCMOSAnalysisDirectory = "gdsc.smlm.sCMOSAnalysisDirectory";
 }
