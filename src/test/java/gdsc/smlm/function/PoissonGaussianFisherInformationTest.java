b2312d5183c6310c370e047581bc504ec5eb9de6
aherbert <a.herbert@sussex.ac.uk>
2018-05-03 13:07:05 +0100

    Notes about the next step to test the function

--- src/test/java/gdsc/smlm/function/PoissonGaussianFisherInformationTest.java
+++ src/test/java/gdsc/smlm/function/PoissonGaussianFisherInformationTest.java
@@ -10,6 +10,13 @@ public class PoissonGaussianFisherInformationTest
 	{
 		PoissonGaussianFisherInformation f = new PoissonGaussianFisherInformation(2, 5);
 
+		// TODO - let the function store the last computed convolution.
+		// Create a plugin to compute the alpha parameter across a range of means.
+		// Draw the P-G convolution and the computed function.
+		// Is it smooth enough for a Simpson sum?
+		// Maybe the simpson sum is not working at low mean as the convolution/function
+		// is not smooth.
+		
 		for (double u = 1; u < 100; u++)
 		{
 			double I = f.getFisherInformation(u);
