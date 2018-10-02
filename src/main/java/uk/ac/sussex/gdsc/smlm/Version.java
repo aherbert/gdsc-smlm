3058a70fe7fbc104e549fa094ee34f7d24380ab1
aherbert <a.herbert@sussex.ac.uk>
2018-10-02 10:16:36 +0100

    Add project build number to Version

--- src/main/java/uk/ac/sussex/gdsc/smlm/Version.java
+++ src/main/java/uk/ac/sussex/gdsc/smlm/Version.java
@@ -25,136 +25,156 @@ package uk.ac.sussex.gdsc.smlm;
 
 import java.io.IOException;
 import java.io.InputStream;
-import java.util.Properties;
+import java.net.URL;
+import java.util.jar.Attributes;
+import java.util.jar.Manifest;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 /**
- * Show the version information contained in the /uk/ac/sussex/gdsc/smlm/Version.txt file.
- * <p>
- * Uses Semantic Versioning.
- *
- * @see <a href="http://semver.org/">http://semver.org/</a>
+ * Show the version information contained in the source jar manifest.
  */
-public class Version
-{
-    /** Constant for the string "unknown" */
-    public static final String UNKNOWN = "unknown";
-    private static String version = null;
-    private static String buildDate = null;
+public class Version {
+  /** Constant for the string "unknown" */
+  public static final String UNKNOWN = "";
+  private static String version = null;
+  private static String buildDate = null;
+  private static String buildNumber = null;
 
-    static
-    {
-        // Locate the version file
-        final Class<Version> resourceClass = Version.class;
-        try (final InputStream propertiesStream = resourceClass
-                .getResourceAsStream("/uk/ac/sussex/gdsc/smlm/Version.txt"))
-        {
-            // Read the version properties
-            final Properties props = new Properties();
-            props.load(propertiesStream);
-            version = props.getProperty("version");
-            buildDate = props.getProperty("build.date");
-        }
-        catch (final IOException e)
-        {
-            // Ignore
-        }
-
-        if (version == null || version.length() == 0)
-            version = UNKNOWN;
-        if (buildDate == null || buildDate.length() == 0)
-            buildDate = UNKNOWN;
+  static {
+    Manifest manifest = loadManifest(Version.class);
+    if (manifest != null) {
+      Attributes attributes = manifest.getMainAttributes();
+      version = attributes.getValue("Specification-Version");
+      buildDate = attributes.getValue("Implementation-Date");
+      buildNumber = attributes.getValue("Implementation-Build");
     }
 
-    /**
-     * The main method. Output the version and build date.
-     *
-     * @param args
-     *            the arguments
-     */
-    public static void main(String[] args)
-    {
-        final StringBuilder msg = new StringBuilder();
-        final String newLine = System.getProperty("line.separator");
-        msg.append("Version : ").append(version).append(newLine);
-        msg.append("Build Date : ").append(buildDate).append(newLine);
-        System.out.print(msg.toString());
-    }
+    if (version == null)
+      version = UNKNOWN;
+    if (buildDate == null)
+      buildDate = UNKNOWN;
+    if (buildNumber == null)
+      buildNumber = UNKNOWN;
+  }
 
-    /**
-     * Get the GDSC SMLM version
-     *
-     * @return The uk.ac.sussex.gdsc.smlm package version
-     */
-    public static String getVersion()
-    {
-        return version;
-    }
+  /**
+   * The main method. Output the version and build date.
+   *
+   * @param args the arguments
+   */
+  public static void main(String[] args) {
+    final StringBuilder msg = new StringBuilder();
+    final String newLine = System.getProperty("line.separator");
+    msg.append("Version : ").append(version).append(newLine);
+    msg.append("Build Date : ").append(buildDate).append(newLine);
+    msg.append("Build Number : ").append(buildNumber).append(newLine);
+    System.out.print(msg);
+  }
 
-    /**
-     * Get the GDSC SMLM package build date
-     *
-     * @return The uk.ac.sussex.gdsc.smlm package build date
-     */
-    public static String getBuildDate()
-    {
-        return buildDate;
-    }
+  /**
+   * Get the GDSC SMLM version
+   *
+   * @return The uk.ac.sussex.gdsc.smlm package version
+   */
+  public static String getVersion() {
+    return version;
+  }
 
-    /**
-     * Get the major version
-     *
-     * @return The major version (or 0 if unknown)
-     */
-    public static int getMajorVersion()
-    {
-        final Pattern p = Pattern.compile("^\\d+");
-        final Matcher m = p.matcher(version);
-        if (m.find())
-            return Integer.parseInt(m.group());
-        return 0;
-    }
+  /**
+   * Get the GDSC SMLM package build date
+   *
+   * @return The uk.ac.sussex.gdsc.smlm package build date
+   */
+  public static String getBuildDate() {
+    return buildDate;
+  }
 
-    /**
-     * Get the minor version
-     *
-     * @return The minor version (or 0 if unknown)
-     */
-    public static int getMinorVersion()
-    {
-        final Pattern p = Pattern.compile("^\\d+\\.(\\d+)");
-        final Matcher m = p.matcher(version);
-        if (m.find())
-            return Integer.parseInt(m.group(1));
-        return 0;
-    }
+  /**
+   * Get the GDSC SMLM package build number
+   *
+   * @return The uk.ac.sussex.gdsc.smlm package build number
+   */
+  public static String getBuildNumber() {
+    return buildNumber;
+  }
 
-    /**
-     * Get the patch version
-     *
-     * @return The patch version (or 0 if unknown)
-     */
-    public static int getPatchVersion()
-    {
-        final Pattern p = Pattern.compile("^\\d+\\.\\d+\\.(\\d+)");
-        final Matcher m = p.matcher(version);
-        if (m.find())
-            return Integer.parseInt(m.group(1));
-        return 0;
-    }
+  /**
+   * Get the major version
+   *
+   * @return The major version (or 0 if unknown)
+   */
+  public static int getMajorVersion() {
+    final Pattern p = Pattern.compile("^\\d+");
+    final Matcher m = p.matcher(version);
+    if (m.find())
+      return Integer.parseInt(m.group());
+    return 0;
+  }
 
-    /**
-     * Get a string with the major, minor and patch versions
-     *
-     * @return Major.Minor.Patch
-     */
-    public static String getMajorMinorPatch()
-    {
-        final Pattern p = Pattern.compile("^\\d+\\.\\d+\\.\\d+");
-        final Matcher m = p.matcher(version);
-        if (m.find())
-            return m.group();
-        return "";
+  /**
+   * Get the minor version
+   *
+   * @return The minor version (or 0 if unknown)
+   */
+  public static int getMinorVersion() {
+    final Pattern p = Pattern.compile("^\\d+\\.(\\d+)");
+    final Matcher m = p.matcher(version);
+    if (m.find())
+      return Integer.parseInt(m.group(1));
+    return 0;
+  }
+
+  /**
+   * Get the patch version
+   *
+   * @return The patch version (or 0 if unknown)
+   */
+  public static int getPatchVersion() {
+    final Pattern p = Pattern.compile("^\\d+\\.\\d+\\.(\\d+)");
+    final Matcher m = p.matcher(version);
+    if (m.find())
+      return Integer.parseInt(m.group(1));
+    return 0;
+  }
+
+  /**
+   * Get a string with the major, minor and patch versions
+   *
+   * @return Major.Minor.Patch
+   */
+  public static String getMajorMinorPatch() {
+    final Pattern p = Pattern.compile("^\\d+\\.\\d+\\.\\d+");
+    final Matcher m = p.matcher(version);
+    if (m.find())
+      return m.group();
+    return "";
+  }
+
+  /**
+   * Load the jar manifest for the given class.
+   *
+   * <p>If not from a jar or an IO exception occurs return null.
+   *
+   * @param clazz the class
+   * @return the manifest (or null)
+   */
+  public static Manifest loadManifest(Class<?> clazz) {
+    String resource = "/" + clazz.getName().replace(".", "/") + ".class";
+    String classPath = clazz.getResource(resource).toString();
+    if (!classPath.startsWith("jar")) {
+      // Class not from JAR
+      return null;
     }
+    String manifestPath =
+        classPath.substring(0, classPath.lastIndexOf('!') + 1) + "/META-INF/MANIFEST.MF";
+    try {
+      try (InputStream in = new URL(manifestPath).openStream()) {
+        return new Manifest(in);
+      }
+    } catch (IOException e) {
+      // Ignore this
+    }
+    return null;
+  }
 }
