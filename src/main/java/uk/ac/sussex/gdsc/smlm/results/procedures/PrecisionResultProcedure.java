b5253746f5a006345705f9c64b169f4ff8360529
aherbert <a.herbert@sussex.ac.uk>
2022-01-25 15:05:38 +0000

    Return stored precisions in native units.
    By convention the units should be nm.

--- src/main/java/uk/ac/sussex/gdsc/smlm/results/procedures/PrecisionResultProcedure.java
+++ src/main/java/uk/ac/sussex/gdsc/smlm/results/procedures/PrecisionResultProcedure.java
@@ -25,9 +25,7 @@
 package uk.ac.sussex.gdsc.smlm.results.procedures;
 
 import uk.ac.sussex.gdsc.core.data.DataException;
-import uk.ac.sussex.gdsc.core.data.utils.TypeConverter;
 import uk.ac.sussex.gdsc.smlm.data.config.FitProtos.PrecisionMethod;
-import uk.ac.sussex.gdsc.smlm.data.config.UnitProtos.DistanceUnit;
 import uk.ac.sussex.gdsc.smlm.results.MemoryPeakResults;
 
 /**
@@ -76,8 +74,8 @@ public class PrecisionResultProcedure extends AbstractResultProcedure implements
    * attempt is made to compute the precision using {@link #getLsePrecision()}. If no exception is
    * thrown then the precision has been computed.
    *
-   * <p>Note: Computed precisions are returned in nm. Any stored precisions are converted to nm for
-   * convenience.
+   * <p>Note: Computed precisions are returned in nm. Any stored precisions are returned
+   * in their native units which by convention should be nm.
    *
    * @param stored the stored flag
    * @return the precision method
@@ -87,11 +85,6 @@ public class PrecisionResultProcedure extends AbstractResultProcedure implements
     if (stored) {
       getStoredPrecision();
       if (results.hasCalibration()) {
-        // Convert to nm for convenience
-        final TypeConverter<DistanceUnit> dc = results.getDistanceConverter(DistanceUnit.NM);
-        for (int i = 0; i < precisions.length; i++) {
-          precisions[i] = dc.convert(precisions[i]);
-        }
         return results.getCalibrationReader().getPrecisionMethod();
       }
       return PrecisionMethod.PRECISION_METHOD_NA;
