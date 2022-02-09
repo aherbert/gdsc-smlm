fe68c078df59c5dc8e7424ee096b0d97328f3985
aherbert <a.herbert@sussex.ac.uk>
2022-02-09 14:49:37 +0000

    Class to compute the log factorial value

--- gdsc-smlm/src/main/java/uk/ac/sussex/gdsc/smlm/function/LogFactorial.java
+++ gdsc-smlm/src/main/java/uk/ac/sussex/gdsc/smlm/function/LogFactorial.java
@@ -24,420 +24,98 @@
 
 package uk.ac.sussex.gdsc.smlm.function;
 
-import java.util.Arrays;
-import java.util.concurrent.locks.ReadWriteLock;
-import java.util.concurrent.locks.ReentrantReadWriteLock;
 import org.apache.commons.math3.special.Gamma;
+import uk.ac.sussex.gdsc.core.utils.ValidationUtils;
 
 /**
- * Compute the log of {@code n!}.
+ * Compute the natural logarithm of the factorial function ({@code ln n!}).
  */
-public class LogFactorial {
+public final class LogFactorial {
+  /** The maximum representable factorial. */
+  private static final int MAX_FACTORIAL = 170;
+  /** ln(sqrt(2 pi)). Computed to 25-digits precision. */
+  private static final double LN_SQRT_TWO_PI = 0.9189385332046727417803297;
+  /** Precomputed factor LANCZOS_G + 1.5. This has a value of 799 / 128. */
+  private static final double LANCZOS_G_PLUS_1_5 = Gamma.LANCZOS_G + 1.5;
+
+  /** No instances. */
+  private LogFactorial() {}
+
   /**
-   * The master table containing {@code log(n!)}.
+   * Get the natural logarithm of the factorial of n.
    *
-   * <p>This should only ever be modified when holding the lock!
+   * @param n argument (must be positive)
+   * @return ln n!
+   * @throws ArrayIndexOutOfBoundsException if n is negative
    */
-  private static volatile double[] masterTable;
-
-  /** All long-representable factorials. */
-  static final long[] FACTORIALS = {1L, 1L, 2L, 6L, 24L, 120L, 720L, 5040L, 40320L, 362880L,
-      3628800L, 39916800L, 479001600L, 6227020800L, 87178291200L, 1307674368000L, 20922789888000L,
-      355687428096000L, 6402373705728000L, 121645100408832000L, 2432902008176640000L};
-
-  static {
-    masterTable = new double[FACTORIALS.length];
-    // Since log(1) == 0 ignore the first two values
-    for (int k = 2; k < FACTORIALS.length; k++) {
-      masterTable[k] = Math.log(FACTORIALS[k]);
+  public static double value(int n) {
+    if (n <= MAX_FACTORIAL) {
+      return Math.log(Factorial.value(n));
     }
+    return logGamma1p(n);
   }
 
   /**
-   * Main lock guarding all access to {@link #masterTable}.
-   */
-  private static final ReadWriteLock lock = new ReentrantReadWriteLock();
-
-  /**
-   * Gets the maximum N that is tabulated.
+   * Get the natural logarithm of the factorial of n as a real number.
    *
-   * <p>Note: This has synchronisation overhead.
+   * <p>This is computed using {@code ln(gamma(1+n))}.
    *
-   * @return the max N
+   * @param n argument (must be positive)
+   * @return ln n!
+   * @throws IllegalArgumentException if n is negative
    */
-  public static int getTableMaxN() {
-    final ReadWriteLock rwl = LogFactorial.lock;
-    rwl.readLock().lock();
-    try {
-      return masterTable.length - 1;
-    } finally {
-      // Unlock read
-      rwl.readLock().unlock();
+  public static double value(double n) {
+    ValidationUtils.checkPositive(n);
+    if (n <= MAX_FACTORIAL && Math.rint(n) == n) {
+      return Math.log(Factorial.value((int) n));
     }
+    return logGamma1p(n);
   }
 
   /**
-   * Increase the tabulated values up to a max n. Does nothing if already above the given n.
+   * Compute {@code log(Gamma(1+x))}.
    *
-   * <p>Note: This has synchronisation overhead.
+   * <p>Adapted from {@code org.apache.commons.math3.special.Gamma} logGamma. Removed support for
+   * NaN and negative x. Updated the code to use x or (x+1) where appropriate.
    *
-   * @param n the n
+   * <p>For {@code x <= 7}, the implementation is based on the double precision implementation in
+   * the <em>NSWC Library of Mathematics Subroutines</em>, {@code DGAMLN}; otherwise the
+   * implementation is based on </p>
+   *
+   * <ul>
+   *
+   * <li><a href="http://mathworld.wolfram.com/GammaFunction.html">Gamma Function</a>, equation
+   * (28).</li>
+   *
+   * <li><a href="https://mathworld.wolfram.com/LanczosApproximation.html"> Lanczos
+   * Approximation</a>, equations (1) through (5).</li>
+   *
+   * <li><a href="http://www.numericana.com/answer/info/godfrey.htm">Paul Godfrey, A note on the
+   * computation of the convergent Lanczos complex Gamma approximation</a></li>
+   *
+   * </ul>
+   *
+   * @param x Argument.
+   * @return the value of {@code log(Gamma(1+x))}
    */
-  public static void increaseTableMaxN(int n) {
-    n = getLowerLimitN(n);
-
-    final ReadWriteLock rwl = LogFactorial.lock;
-    rwl.readLock().lock();
-    if (masterTable.length <= n) {
-      // Must release read lock before acquiring write lock
-      rwl.readLock().unlock();
-      rwl.writeLock().lock();
-      try {
-        // Recheck state because another thread might have
-        // acquired write lock and changed state before we did.
-        if (masterTable.length <= n) {
-          masterTable = changeSize(masterTable, n);
-        }
-        // Downgrade by acquiring read lock before releasing write lock
-        rwl.readLock().lock();
-      } finally {
-        rwl.writeLock().unlock(); // Unlock write, still hold read
+  private static double logGamma1p(double x) {
+    if (x <= 1.5) {
+      if (x == 0) {
+        return 0;
       }
+      return -Math.log1p(Gamma.invGamma1pm1(x));
     }
-    // Unlock read
-    rwl.readLock().unlock();
-  }
-
-  /**
-   * Change the size of the table to tabulate up to a maximum of n.
-   *
-   * @param table the table
-   * @param n the n
-   * @return the new table (length = n+1)
-   */
-  private static double[] changeSize(double[] table, int n) {
-    final double[] newTable = Arrays.copyOf(table, n + 1);
-
-    // Using gamma for consistency with non-tabulated values
-    int ki = table.length - 1;
-    while (ki < n) {
-      ki++;
-      newTable[ki] = Gamma.logGamma(ki + 1.0);
-    }
-
-    return newTable;
-  }
-
-  /**
-   * Reduces the tabulated values down to a max n. Does nothing if already below the given n.
-   *
-   * <p>Note: This has synchronisation overhead.
-   *
-   * @param n the new table max N
-   */
-  public static void reduceTableMaxN(int n) {
-    final int n1 = getLowerLimitN(n) + 1;
-
-    final ReadWriteLock rwl = LogFactorial.lock;
-    rwl.readLock().lock();
-    if (masterTable.length > n1) {
-      // Must release read lock before acquiring write lock
-      rwl.readLock().unlock();
-      rwl.writeLock().lock();
-      try {
-        // Recheck state because another thread might have
-        // acquired write lock and changed state before we did.
-        if (masterTable.length > n1) {
-          masterTable = changeSize(masterTable, n);
-        }
-        // Downgrade by acquiring read lock before releasing write lock
-        rwl.readLock().lock();
-      } finally {
-        rwl.writeLock().unlock(); // Unlock write, still hold read
+    if (x <= 7) {
+      final int n = (int) Math.floor(x - 0.5);
+      double prod = 1;
+      for (int i = 0; i < n; i++) {
+        prod *= x - i;
       }
+      return Math.log(prod) - Math.log1p(Gamma.invGamma1pm1(x - n));
     }
-
-    // Unlock read
-    rwl.readLock().unlock();
-  }
-
-  /**
-   * Gets the lower limit on N in order to keep the representable factorials.
-   *
-   * @param n the n
-   * @return the lower limit
-   */
-  private static int getLowerLimitN(int n) {
-    return Math.max(n, FACTORIALS.length - 1);
-  }
-
-  /**
-   * Compute the log of n!. Uses tabulated values or the gamma function if n is large.
-   *
-   * <p>Note: This has no synchronisation overhead.
-   *
-   * @param n the value n (must be positive)
-   * @return log(n!)
-   * @throws IndexOutOfBoundsException if n is negative
-   */
-  public static double logF(int n) {
-    // This is not synchronized.
-    // We read a copy of the table from main memory.
-    // If it is too small then just compute using the Gamma function.
-    final double[] logF = masterTable;
-    if (n < logF.length) {
-      return logF[n];
-    }
-    return Gamma.logGamma(n + 1.0);
-  }
-
-  /**
-   * Compute the log of n!. Uses the gamma function.
-   *
-   * <p>Note: This has no synchronisation overhead.
-   *
-   * @param n the value n
-   * @return log(n!)
-   */
-  public static double logF(double n) {
-    if (n <= 1) {
-      return 0;
-    }
-    return Gamma.logGamma(n + 1);
-  }
-
-  /////////////////////////////////////
-  // Instance with pre-computed values
-  /////////////////////////////////////
-
-  private volatile double[] objectTable;
-
-  /**
-   * Main lock guarding all access to {@link #objectTable}.
-   */
-  private final ReadWriteLock objectLock = new ReentrantReadWriteLock();
-
-  /**
-   * Instantiates a new log factorial using the current static table.
-   *
-   * <p>The size of the static table can be obtained using {@link #getTableMaxN()}. Note that the
-   * table may be changed by other threads. The size of the newly constructed object can be obtained
-   * using {@link #getMaxN()}.
-   */
-  public LogFactorial() {
-    // Copy the static values already present
-    objectTable = LogFactorial.masterTable;
-  }
-
-  /**
-   * Instantiates a new log factorial.
-   *
-   * @param objectTable the object table
-   */
-  private LogFactorial(double[] objectTable) {
-    this.objectTable = objectTable;
-  }
-
-  /**
-   * Instantiates a new log factorial using the given table size.
-   *
-   * @param n the n
-   */
-  public LogFactorial(int n) {
-    // Copy the static values already present
-    objectTable = changeSize(LogFactorial.masterTable, getLowerLimitN(n));
-  }
-
-  /**
-   * Gets the maximum N that is tabulated.
-   *
-   * <p>Note: This has synchronisation overhead.
-   *
-   * @return the max N
-   */
-  public int getMaxN() {
-    final ReadWriteLock rwl = this.objectLock;
-    rwl.readLock().lock();
-    try {
-      return objectTable.length - 1;
-    } finally {
-      // Unlock read
-      rwl.readLock().unlock();
-    }
-  }
-
-  /**
-   * Increase the tabulated values up to a max n. Does nothing if already above the given n.
-   *
-   * <p>Note: This has synchronisation overhead.
-   *
-   * @param n the n
-   */
-  public void increaseMaxN(int n) {
-    n = getLowerLimitN(n);
-
-    final ReadWriteLock rwl = LogFactorial.lock;
-    rwl.readLock().lock();
-    if (objectTable.length <= n) {
-      // Must release read lock before acquiring write lock
-      rwl.readLock().unlock();
-      rwl.writeLock().lock();
-      try {
-        // Recheck state because another thread might have
-        // acquired write lock and changed state before we did.
-        if (objectTable.length <= n) {
-          // Use the master table if it is bigger as that has all values pre-computed.
-          final double[] mtable = LogFactorial.masterTable;
-          if (mtable.length > objectTable.length) {
-            objectTable = Arrays.copyOf(mtable, Math.min(n + 1, mtable.length));
-          }
-
-          if (objectTable.length <= n) {
-            objectTable = changeSize(objectTable, n);
-          }
-        }
-        // Downgrade by acquiring read lock before releasing write lock
-        rwl.readLock().lock();
-      } finally {
-        rwl.writeLock().unlock(); // Unlock write, still hold read
-      }
-    }
-
-    // Unlock read
-    rwl.readLock().unlock();
-  }
-
-  /**
-   * Ensure the table contains values for the specified range of N.
-   *
-   * <p>This can be called before using the object with a known range of n.
-   *
-   * <p>Note: This has synchronisation overhead.
-   *
-   * @param minN the min N
-   * @param maxN the max N
-   * @throws IllegalArgumentException If min is greater than max
-   */
-  public void ensureRange(int minN, int maxN) {
-    // Validate the range
-    if (minN < 0) {
-      minN = 0;
-    }
-    maxN = getLowerLimitN(maxN);
-    if (minN > maxN) {
-      throw new IllegalArgumentException("Max must be greater than min");
-    }
-
-    final ReadWriteLock rwl = LogFactorial.lock;
-    rwl.readLock().lock();
-    if (objectTable.length <= maxN || !checkRange(minN, maxN)) {
-      // Must release read lock before acquiring write lock
-      rwl.readLock().unlock();
-      rwl.writeLock().lock();
-      try {
-        // Recheck state because another thread might have
-        // acquired write lock and changed state before we did.
-        if (objectTable.length <= maxN) {
-          // Use the master table if it is bigger as that has all values pre-computed.
-          final double[] mtable = LogFactorial.masterTable;
-          objectTable =
-              Arrays.copyOf((mtable.length > objectTable.length) ? mtable : objectTable, maxN + 1);
-        }
-
-        // Check range has pre-computed values
-        for (int n = Math.max(2, minN); n <= maxN; n++) {
-          if (objectTable[n] == 0) {
-            // Already hold the write lock so just write the values
-            objectTable[n] = Gamma.logGamma(n + 1.0);
-          }
-        }
-
-        // Downgrade by acquiring read lock before releasing write lock
-        rwl.readLock().lock();
-      } finally {
-        rwl.writeLock().unlock(); // Unlock write, still hold read
-      }
-    }
-
-    // Unlock read
-    rwl.readLock().unlock();
-  }
-
-  /**
-   * Check the table has computed values in the range minN to maxN inclusive.
-   *
-   * <p>Only call when holding the read lock.
-   *
-   * @param minN the min N
-   * @param maxN the max N
-   * @return true, if successful
-   */
-  private boolean checkRange(int minN, int maxN) {
-    for (int n = Math.max(2, minN); n <= maxN; n++) {
-      if (objectTable[n] == 0) {
-        return false;
-      }
-    }
-    return true;
-  }
-
-  /**
-   * Get the log of n! using tabulated values.
-   *
-   * <p>Note: This has no synchronisation overhead.
-   *
-   * @param n the n (must be positive)
-   * @return log(n!)
-   * @throws ArrayIndexOutOfBoundsException if n is outside the table bounds
-   * @see #getMaxN()
-   */
-  public double getLogF(int n) {
-    final double value = objectTable[n];
-    return (value == 0) ? LogFactorial.logF(n) : value;
-  }
-
-  /**
-   * Get the log of n! using tabulated values.
-   *
-   * <p>Note: This has no synchronisation overhead.
-   *
-   * <p>No checks are made that the object table contains a pre-computed value, for instance in the
-   * case where the table was created using {@link #ensureRange(int, int)} there may be values
-   * outside the range that are zero.
-   *
-   * @param n the n (must be positive)
-   * @return log(n!)
-   * @throws ArrayIndexOutOfBoundsException if n is outside the table bounds
-   * @see #getMaxN()
-   */
-  public double getLogFUnsafe(int n) {
-    return objectTable[n];
-  }
-
-  /**
-   * Copy the object.
-   *
-   * @return the log factorial
-   */
-  public LogFactorial copy() {
-    return new LogFactorial(objectTable);
-  }
-
-  /**
-   * Copy the object with the given table size.
-   *
-   * <p>If n is larger then the current size then the table will be expanded.
-   *
-   * @param n the n
-   * @return the log factorial
-   */
-  public LogFactorial withN(int n) {
-    // Copy the values already present
-    final double[] mtable = LogFactorial.masterTable;
-    double[] otable = this.objectTable;
-    if (mtable.length > otable.length) {
-      otable = mtable;
-    }
-    return new LogFactorial(changeSize(otable, getLowerLimitN(n)));
+    final double x1p = x + 1;
+    final double sum = Gamma.lanczos(x1p);
+    final double tmp = x + LANCZOS_G_PLUS_1_5;
+    return (x + 1.5) * Math.log(tmp) - tmp + LN_SQRT_TWO_PI + Math.log(sum / x1p);
   }
 }
---
+++ gdsc-smlm/src/test/java/uk/ac/sussex/gdsc/smlm/function/LogFactorialTest.java
@@ -0,0 +1,144 @@
+/*-
+ * #%L
+ * Genome Damage and Stability Centre SMLM ImageJ Plugins
+ *
+ * Software for single molecule localisation microscopy (SMLM)
+ * %%
+ * Copyright (C) 2011 - 2022 Alex Herbert
+ * %%
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public
+ * License along with this program.  If not, see
+ * <http://www.gnu.org/licenses/gpl-3.0.html>.
+ * #L%
+  */
+
+package uk.ac.sussex.gdsc.smlm.function;
+
+import java.math.BigInteger;
+import java.util.ArrayList;
+import org.apache.commons.math3.special.Gamma;
+import org.apache.commons.rng.UniformRandomProvider;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import uk.ac.sussex.gdsc.test.api.TestAssertions;
+import uk.ac.sussex.gdsc.test.api.TestHelper;
+import uk.ac.sussex.gdsc.test.api.function.DoubleDoubleBiPredicate;
+import uk.ac.sussex.gdsc.test.junit5.RandomSeed;
+import uk.ac.sussex.gdsc.test.junit5.SeededTest;
+import uk.ac.sussex.gdsc.test.rng.RngUtils;
+
+@SuppressWarnings({"javadoc"})
+class LogFactorialTest {
+  /** The largest representable factorial using a double (n=170). */
+  private static final int MAX_N = 170;
+
+  @Test
+  void testZero() {
+    Assertions.assertEquals(0, LogFactorial.value(0), "0!");
+    Assertions.assertEquals(0, LogFactorial.value(0.0), "0!");
+  }
+
+  @Test
+  void testIllegalArgumentThrows() {
+    // For performance this uses an IOOB exception
+    Assertions.assertThrows(IndexOutOfBoundsException.class, () -> LogFactorial.value(-1));
+    Assertions.assertThrows(IllegalArgumentException.class, () -> LogFactorial.value(-0.5));
+    Assertions.assertThrows(IllegalArgumentException.class, () -> LogFactorial.value(Double.NaN));
+  }
+
+  @Test
+  void testLogFactorialInteger() {
+    // Start at 0!
+    BigInteger value = BigInteger.ONE;
+    final ArrayList<BigInteger> values = new ArrayList<>(MAX_N + 1);
+    values.add(value);
+    for (int n = 1; n <= MAX_N; n++) {
+      // n! = (n-1)! * n
+      value = value.multiply(BigInteger.valueOf(n));
+      values.add(value);
+      final double expected = Math.log(value.doubleValue());
+      Assertions.assertEquals(expected, LogFactorial.value(n));
+      Assertions.assertEquals(expected, LogFactorial.value((double) n));
+    }
+
+    // Compute some more by log summation
+    double base = Math.log(value.doubleValue());
+    final DoubleDoubleBiPredicate tol = TestHelper.doublesAreClose(1e-15);
+    for (int n = MAX_N + 1; n < MAX_N * 3; n++) {
+      base += Math.log(n);
+      final double v1 = LogFactorial.value(n);
+      final double v2 = LogFactorial.value((double) n);
+      Assertions.assertEquals(v1, v2);
+      TestAssertions.assertTest(base, v1, tol);
+      // Prevent drift in the summation
+      base = v1;
+    }
+  }
+
+  /**
+   * Test the factorial of a fractional number against Commons Math logGamma(1+n).
+   */
+  @SeededTest
+  void testLogFactorialDouble(RandomSeed seed) {
+    final UniformRandomProvider rng = RngUtils.create(seed.getSeed());
+    final DoubleDoubleBiPredicate tol = TestHelper.doublesAreClose(1e-14);
+    for (int i = 0; i < 200; i++) {
+      final double n = rng.nextDouble() * 200;
+      final double expected = n <= 1.5 ? Gamma.logGamma1p(n) : Gamma.logGamma(1 + n);
+      TestAssertions.assertTest(expected, LogFactorial.value(n), tol, () -> Double.toString(n));
+    }
+  }
+
+  /**
+   * Test the factorial of a fractional number against reference values.
+   */
+  @ParameterizedTest
+  @CsvSource({
+      // Computed using Matlab gammaln(1+n) with long double precision
+      // @formatter:off
+      "0.25, -0.098271836421813169",
+      "0.75, -0.084401121020485553",
+      "1.125, 0.057759851530343874",
+      "1.75, 0.47521466691493708",
+      "2.75, 1.4868155785934172",
+      "3.25, 2.1144569274503713",
+      "4.75, 4.3667160366222859",
+      "6.25, 7.0521854507385395",
+      "12.75, 21.903762491828793",
+      "17.125, 33.86331080630174",
+      "27.75, 67.053353891702798",
+      "57.75, 179.43956662288721",
+      "97.75, 353.39188783368269",
+      "137.75, 544.11168543338499",
+      "154.25, 626.38890784701778",
+      "164.25, 677.12339194008723",
+      "170.25, 707.85792962271012",
+      "170.624, 709.78077443669906",
+      // Limit
+      "170.6243769563027, 709.78271289338397",
+      // Infinite for n!
+      "170.62437695630274, 709.78271289338409",
+      "170.625, 709.78591682948354",
+      "171.5, 714.28774629753423",
+      "201.25, 869.86189472479612",
+      "271.25, 1252.2956116469838",
+      "411.25, 2068.075277298678",
+      // @formatter:on
+  })
+  void testLogFactorial(double n, double expected) {
+    final DoubleDoubleBiPredicate tol = TestHelper.doublesAreClose(1e-15);
+    TestAssertions.assertTest(expected, LogFactorial.value(n), tol, () -> Double.toString(n));
+  }
+}
