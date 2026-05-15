e839079be9c7f9cba5ed6d4cf3b5aab45987ef48
Alex Herbert <a.herbert@sussex.ac.uk>
2026-05-15 18:14:14 +0100

    Add analysis functions for diffusion in a depth-of-field

---
+++ gdsc-smlm/src/main/java/uk/ac/sussex/gdsc/smlm/fitting/DiffusionAnalysis.java
@@ -0,0 +1,124 @@
+/*-
+ * #%L
+ * Genome Damage and Stability Centre SMLM Package
+ *
+ * Software for single molecule localisation microscopy (SMLM)
+ * %%
+ * Copyright (C) 2011 - 2025 Alex Herbert
+ * %%
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public
+ * License along with this program.  If not, see
+ * <http://www.gnu.org/licenses/gpl-3.0.html>.
+ * #L%
+ */
+
+package uk.ac.sussex.gdsc.smlm.fitting;
+
+import org.apache.commons.math3.analysis.UnivariateFunction;
+import org.apache.commons.math3.exception.TooManyEvaluationsException;
+import org.apache.commons.numbers.gamma.Erfc;
+import uk.ac.sussex.gdsc.core.data.VisibleForTesting;
+import uk.ac.sussex.gdsc.smlm.math3.analysis.integration.CustomSimpsonIntegrator;
+
+/**
+ * Perform diffusion analysis.
+ *
+ * <p>Contains methods that compute the probability that a diffusing molecule remains within the
+ * depth-of-field after a given time. This is based on the Spot-On model described in the
+ * methods section of the paper:
+ *
+ * <p>Hansen, A.S., Woringer, M., Grimm, J.B., Lavis, L.D., Tjian, R., and Darzacq, X. (2018) Robust
+ * model-based analysis of single-particle tracking experiments with Spot-On. eLife 7, e33125.
+ * doi:10.7554/eLife.33125.
+ */
+public class DiffusionAnalysis {
+  private DiffusionAnalysis() {}
+
+  /**
+   * Compute the probability that a molecule remains within bounds {@code [-dz/2, dz/2]} when
+   * diffusing with a coefficient {@code d} for time {@code dt}.
+   *
+   * @param dt the time delay (must be strictly positive)
+   * @param dz the depth of field
+   * @param d the diffusion coefficient
+   * @return the probability
+   */
+  static double remaining(double dt, double dz, double d) {
+    // The function is symmetric about z=0.
+    // Integrate [0, z/2].
+    // Each withinBound function is accurate to 1e-10 and the result is in [0, dz/2].
+    // Use a maximum of n=200 evaluations.
+    // When the molecule is diffusing fast out of bounds the evaluations is far less
+    // using an integrator. If this fails we just use the last known result which is
+    // more accurate than a sum of the same number of evenly space points.
+    final int n = 200;
+    final CustomSimpsonIntegrator in = new CustomSimpsonIntegrator(1e-8, 1e-8, 3, 63);
+    final double denom = 1.0 / Math.sqrt(4 * d * dt);
+    final UnivariateFunction fun = z -> DiffusionAnalysis.withinBound(z, dz, denom);
+    try {
+      in.integrate(n, fun, 0, dz * 0.5);
+    } catch (TooManyEvaluationsException ignored) {
+      // Ignore this.
+      // Allow other exceptions to trickle up as they are actual errors in configuration.
+    }
+    // result in [0, 1]
+    return 2 * in.getLastSum() / dz;
+  }
+
+  /**
+   * Compute the probability that a molecule at position {@code z} remains within bounds
+   * {@code [-dz/2, dz/2]} when diffusing with a coefficient {@code d} for time {@code dt}.
+   *
+   * @param z z position
+   * @param dt the time delay (must be strictly positive)
+   * @param dz the depth of field
+   * @param d the diffusion coefficient
+   * @return the probability
+   */
+  @VisibleForTesting
+  static double withinBound(double z, double dt, double dz, double d) {
+    final double denom = 1.0 / Math.sqrt(4 * d * dt);
+    return withinBound(z, dz, denom);
+  }
+
+  /**
+   * Compute the probability that a molecule at position {@code z} remains within bounds
+   * {@code [-dz/2, dz/2]} when diffusing with a coefficient {@code d} for time {@code dt}.
+   *
+   * @param z z position (in range [-dz/2, dz/2])
+   * @param dz the depth of field
+   * @param denom 1 / sqrt(4*d*dt)
+   * @return the probability
+   */
+  private static double withinBound(double z, double dz, double denom) {
+    // As per Spot-On:
+    // terms of the series evaluated until below 1e-10
+    int n = 0;
+    double sum = 0;
+    double term;
+    do {
+      // erfc [ ((2n+1)dz / 2) - z ] / sqrt(4*d*dt)
+      // erfc [ ((2n+1)dz / 2) + z ] / sqrt(4*d*dt)
+      final double lo = (((n + 0.5) * dz) - z) * denom;
+      final double hi = (((n + 0.5) * dz) + z) * denom;
+      term = Erfc.value(lo) + Erfc.value(hi);
+      // Alternating sum: (-1)^n * term
+      sum += (n & 1) == 0 ? term : -term;
+      n++;
+    } while (term > 1e-10);
+    // As |z| approaches dz/2 the sum approaches one.
+    // Do not allow summation error of the alternating terms
+    // of descending magnitude to exceed 1.
+    return sum > 1 ? 0 : 1 - sum;
+  }
+}
---
+++ gdsc-smlm/src/test/java/uk/ac/sussex/gdsc/smlm/fitting/DiffusionAnalysisTest.java
@@ -0,0 +1,75 @@
+/*-
+ * #%L
+ * Genome Damage and Stability Centre SMLM Package
+ *
+ * Software for single molecule localisation microscopy (SMLM)
+ * %%
+ * Copyright (C) 2011 - 2025 Alex Herbert
+ * %%
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public
+ * License along with this program.  If not, see
+ * <http://www.gnu.org/licenses/gpl-3.0.html>.
+ * #L%
+  */
+
+package uk.ac.sussex.gdsc.smlm.fitting;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import uk.ac.sussex.gdsc.test.api.Predicates;
+import uk.ac.sussex.gdsc.test.api.TestAssertions;
+
+@SuppressWarnings({"javadoc"})
+class DiffusionAnalysisTest {
+  @ParameterizedTest
+  @CsvSource({
+    // Data obtained from the python code:
+    // https://gitlab.com/tjian-darzacq-lab/Spot-On-cli
+    // fastspt.fastspt.C_AbsorBoundAUTO
+    "0.4409547738693467, 0.01, 1.5, 1.0, 0.9711316588468534",
+    "0.7424623115577889, 0.01, 1.5, 1.0, 0.042506726296412944",
+    "-0.7047738693467337, 0.02, 1.5, 1.0, 0.1789001958617121",
+    "-0.2242462311557789, 0.03, 0.75, 1.0, 0.447549370861025",
+    "0.3071608040201005, 0.03, 0.75, 1.5, 0.16233614943364597",
+    "0.32223618090452255, 0.03, 0.75, 0.25, 0.3333969348168449",
+    "0.28831658291457285, 0.03, 0.75, 0.025, 0.9747891295238313",
+    // Boundary case.
+    // Evaluates as -5.009431107187975e-17 so we clip to zero.
+    "0.375, 0.03, 0.75, 1.5, 0",
+  })
+  void testWithinBound(double z, double dt, double dz, double d, double p) {
+    TestAssertions.assertTest(p, DiffusionAnalysis.withinBound(z, dt, dz, d),
+        Predicates.doublesAreRelativelyClose(1e-10));
+  }
+
+  @ParameterizedTest
+  @CsvSource({
+    // Data obtained from the python code:
+    // https://gitlab.com/tjian-darzacq-lab/Spot-On-cli
+    // Using integration of: fastspt.fastspt.C_AbsorBoundAUTO
+    // Integration performed using scipy.integrate.quad
+    // using default epsabs=1.49e-08, epsrel=1.49e-08.
+    "0.03, 0.75, 0.025, 0.9175948369017197, 1e-7",
+    "0.03, 0.75, 0.25, 0.7394119937446663, 1e-9",
+    "0.01, 0.75, 2.5, 0.5244804117348121, 1e-8",
+    "0.01, 0.75, 10, 0.14020993007941418, 1e-8",
+    "0.02, 1.25, 5, 0.43129485401041096, 1e-8",
+    // Cases requiring more than n=200 evaluations are not as accurate
+    "0.01, 1.75, 0.025, 0.9796100101011112, 1e-5",
+    "0.001, 1.75, 0.02, 0.994232839949668, 1e-3",
+  })
+  void testRemaining(double dt, double dz, double d, double p, double eps) {
+    TestAssertions.assertTest(p, DiffusionAnalysis.remaining(dt, dz, d),
+        Predicates.doublesAreRelativelyClose(eps));
+  }
+}
